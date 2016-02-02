fbf25378d29c8abbe0549a9ebc45853605a9bddd
Rye Terrell <ryeterrell@ryeterrell.net>
2016-02-01 20:02:59 -0600

    reorg for gh-pages

--- build.sh
+++
@@ -1,7 +0,0 @@
-cd build/
-rm LumaGL.zip
-node build.js > LumaGL.js
-java -jar compiler.jar --js LumaGL.js --js_output_file LumaGL.cls.js
-cd ../
-git archive --format zip --output ./build/LumaGL.zip master 
-
