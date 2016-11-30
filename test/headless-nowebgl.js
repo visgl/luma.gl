4617ca0579d851be01e7d82d728f1723f528a0c3
Ib Green <ibgreen@users.noreply.github.com>
2016-11-30 13:09:46 -0800

    Add test case and fix for glGetDebugInfo regression under Node. (#111)
    * Add test case and fix for glGetDebugInfo regression under Node.
    
    * Add "experimental.js" to exported "files"  in package.json.
    
    * Add comments to some top-level test index.js files

--- test/headless-nowebgl.js
+++ test/headless-nowebgl.js
@@ -1,6 +1,9 @@
+// Note that we do two test runs on luma.gl, with and without headless-gl
+// This file imports tests that should run *without* headless-gl included
+
 require('babel-core/register');
 require('babel-polyfill');
 
 // Quick test that webgl independent code works
-require('./webgl/core-spec');
+require('./webgl/context-no-headless.spec');
 require('./webgl-independent');
--- test/webgl/index.js
+++ test/webgl/index.js
@@ -1,16 +1,19 @@
+// Note that we do two test runs on luma.gl, with and without headless-gl
+// This file imports tests that should run *with* headless-gl included
+
+// helpers & utils
 import './helpers/query-manager.spec';
 
 // webgl
-import './webgl-spec';
-import './core-spec.js';
-import './vertex-attributes-spec';
-import './buffer-spec';
-import './program-spec';
-import './texture-spec';
-import './framebuffer-spec';
-import './renderbuffer-spec';
-import './draw-spec';
-import './uniforms-spec';
+import './context.spec.js';
+import './vertex-attributes.spec';
+import './buffer.spec';
+import './program.spec';
+import './texture.spec';
+import './framebuffer.spec';
+import './renderbuffer.spec';
+import './draw.spec';
+import './uniforms.spec';
 
 // Extensions
 import './timer-query.spec';
@@ -18,7 +21,7 @@ import './timer-query.spec';
 // import './fbo-spec';
 
 // webgl2
-import './vertex-array-object-spec';
+import './vertex-array-object.spec';
 
 // misc
 import './io-spec';
