1e07d0f6e707788aae4e957a591553d0fcc37b1a
Ib Green <ibgreen@users.noreply.github.com>
2017-02-15 11:37:38 -0800

    Removed _docs/examples/wip (#126)

--- _docs/examples/wip/wave/shaders/back.vs.glsl
+++
@@ -1,9 +0,0 @@
-attribute vec3 position;
-varying vec4 vPosition;
-uniform mat4 worldMatrix;
-uniform mat4 projectionMatrix;
-
-void main(void) {
-  vPosition = vec4(position, 1);
-  gl_Position = projectionMatrix * worldMatrix * vPosition;
-}
