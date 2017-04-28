fab5bff19bc3af9d9f783dd272cdbc2273bc37bb
Ib Green <ib@uber.com>
2017-04-28 14:23:44 -0700

    Update buffer docs

--- src/webgl/buffer.js
+++ src/webgl/buffer.js
@@ -28,7 +28,7 @@ export class BufferLayout {
    * @param {GLuint} offset=0 - supports strided arrays
    */
   constructor({
-    // Characteristics of stored data
+    // Characteristics of stored data,
     type,
     size = 1,
     offset = 0,
@@ -48,102 +48,21 @@ export class BufferLayout {
 }
 
 export default class Buffer extends Resource {
-
-  /*
-   * @classdesc
-   * Can be used to store vertex data, pixel data retrieved from images
-   * or the framebuffer, and a variety of other things.
-   *
-   * Mainly used for uploading VertexAttributes to GPU
-   * Setting data on a buffers (arrays) uploads it to the GPU.
-   *
-   * Holds an attribute name as a convenience
-   * setData - Initializes size of buffer and sets
-   *
-   * @param {WebGLRenderingContext} gl - gl context
-   * @param {string} opt.id - id for debugging
-   */
   constructor(gl, opts = {}) {
     super(gl, opts);
-    this.target = null;
+    // In WebGL1, we need to make sure we use GL.ELEMENT_ARRAY_BUFFER when
+    // initializing element buffers, otherwise the buffer type will be locked
+    // to a generic (non-element) buffer.
+    // In WebGL2, we can use GL.COPY_READ_BUFFER which avoids locking the type
+    this.target = opts.target || (this.gl.webgl2 ? GL.COPY_READ_BUFFER : GL.ARRAY_BUFFER);
     this.index = null;
     this.setData(opts);
     Object.seal(this);
   }
 
-  /**
-   * Updates a data buffer, reallocating if necessary
-   */
-  update({data}) {
-    if (data.byteLength > this.bytes) {
-      this.setData({data});
-    } else {
-      this.subData({data});
-    }
-  }
-
-  /**
-   * Creates and initializes the buffer object's data store.
-   *
-   * @param {ArrayBufferView} opt.data - contents
-   * @param {GLsizeiptr} opt.bytes - the size of the buffer object's data store.
-   * @param {GLenum} opt.usage=gl.STATIC_DRAW - Allocation hint for GPU driver
-   *
-   * Characteristics of stored data, hints for vertex attribute
-   *
-   * @param {GLenum} opt.dataType=gl.FLOAT - type of data stored in buffer
-   * @param {GLuint} opt.size=1 - number of values per vertex
-   * @returns {Buffer} Returns itself for chaining.
-   */
-  setData({
-    target,
-    index,
-    data,
-    bytes,
-    usage = GL.STATIC_DRAW,
-    // Layout of stored data
-    layout,
-    type,
-    size = 1,
-    offset = 0,
-    stride = 0,
-    normalized = false,
-    integer = false,
-    instanced = 0
-  } = {}) {
-    const opts = arguments[0];
-
-    this.setDefaultTarget(opts);
-
-    if (!data) {
-      bytes = bytes || 0;
-    } else {
-      type = type || getGLTypeFromTypedArray(data);
-      bytes = data.byteLength;
-      assert(type, ERR_BUFFER_PARAMS);
-    }
-
-    this.bytes = bytes;
-    this.bytesUsed = bytes;
-    this.data = data;
-    this.type = type;
-    this.usage = usage;
-
-    // Call after type is set
-    this.setDataLayout(Object.assign(opts));
-
-    // Create the buffer - binding it here for the first time locks the type
-    target = this.target || GL.ARRAY_BUFFER;
-    this.gl.bindBuffer(target, this.handle);
-    this.gl.bufferData(target, data || bytes, usage);
-    this.gl.bindBuffer(target, null);
-
-    return this;
-  }
-
   /*
    * Stores the layout of data with the buffer which makes it easy to
-   * set it as an attribute later
+   * e.g. set it as an attribute later
    */
   setDataLayout({
     layout,
@@ -164,6 +83,62 @@ export default class Buffer extends Resource {
       integer,
       instanced
     });
+    return this;
+  }
+
+  /**
+   * Creates and initializes the buffer object's data store.
+   *
+   * @param {ArrayBufferView} opt.data - contents
+   * @param {GLsizeiptr} opt.bytes - the size of the buffer object's data store.
+   * @param {GLenum} opt.usage=gl.STATIC_DRAW - Allocation hint for GPU driver
+   *
+   * Characteristics of stored data, hints for vertex attribute
+   *
+   * @param {GLenum} opt.dataType=gl.FLOAT - type of data stored in buffer
+   * @param {GLuint} opt.size=1 - number of values per vertex
+   * @returns {Buffer} Returns itself for chaining.
+   */
+  setData({
+    data,
+    bytes,
+    usage = GL.STATIC_DRAW,
+    // Layout of stored data
+    layout,
+    type,
+    size = 1,
+    offset = 0,
+    stride = 0,
+    normalized = false,
+    integer = false,
+    instanced = 0
+  } = {}) {
+    const opts = arguments[0];
+
+    if (!data) {
+      bytes = bytes || 0;
+    } else {
+      type = type || getGLTypeFromTypedArray(data);
+      bytes = data.byteLength;
+      assert(type, ERR_BUFFER_PARAMS);
+    }
+
+    this.bytes = bytes;
+    this.bytesUsed = bytes;
+    this.data = data;
+    this.type = type;
+    this.usage = usage;
+
+    // Call after type is set
+    this.setDataLayout(Object.assign(opts));
+
+    // Create the buffer - binding it here for the first time locks the type
+    // In WebGL2, use GL.COPY_WRITE_BUFFER to avoid locking the type
+    const target = this.gl.webgl2 ? GL.COPY_WRITE_BUFFER : this.target;
+    this.gl.bindBuffer(target, this.handle);
+    this.gl.bufferData(target, data || bytes, usage);
+    this.gl.bindBuffer(target, null);
+
     return this;
   }
 
@@ -189,9 +164,12 @@ export default class Buffer extends Resource {
       return this;
     }
 
-    this.gl.bindBuffer(this.target, this.handle);
-    this.gl.bufferSubData(this.target, offset, data);
-    this.gl.bindBuffer(this.target, null);
+    // Create the buffer - binding it here for the first time locks the type
+    // In WebGL2, use GL.COPY_WRITE_BUFFER to avoid locking the type
+    const target = this.gl.webgl2 ? GL.COPY_WRITE_BUFFER : this.target;
+    this.gl.bindBuffer(target, this.handle);
+    this.gl.bufferSubData(target, offset, data);
+    this.gl.bindBuffer(target, null);
     return this;
   }
 
@@ -216,7 +194,8 @@ export default class Buffer extends Resource {
     size
   }) {
     assertWebGL2Context(this.gl);
-    // GL.COPY_READ_BUFFER GL.COPY_WRITE_BUFFER exist to avoid disturbing other targets, so use them
+
+    // Use GL.COPY_READ_BUFFER+GL.COPY_WRITE_BUFFER avoid disturbing other targets and locking type
     this.gl.bindBuffer(GL.COPY_READ_BUFFER, sourceBuffer.handle);
     this.gl.bindBuffer(GL.COPY_WRITE_BUFFER, this.handle);
 
@@ -249,23 +228,6 @@ export default class Buffer extends Resource {
     this.gl.getBufferSubData(this.target, srcByteOffset, dstData, dstOffset, length);
     this.gl.bindBuffer(GL.COPY_READ_BUFFER, null);
     return dstData;
-  }
-
-  /**
-   * Sets the default target of the buffer for bind operations
-   *
-   * Note: Cannot change between ELEMENT_ARRAY_BUFFER and other targets:
-   * In the WebGL API, a given buffer object may only be bound to one of the
-   * other data or ELEMENT_ARRAY_BUFFER binding points in its lifetime.
-   * This restriction implies that a given buffer object may contain either
-   * vertices/data or indices, but not both.
-   */
-  setDefaultTarget({target, index}) {
-    // If target is supplied, overwrite it
-    if (target) {
-      this.target = target;
-      this.index = index;
-    }
   }
 
   /**
@@ -311,12 +273,26 @@ export default class Buffer extends Resource {
 
   unbind({target = this.target, index = this.index} = {}) {
     const isIndexedBuffer = target === GL.UNIFORM_BUFFER || target === GL.TRANSFORM_FEEDBACK_BUFFER;
-    if (!isIndexedBuffer) {
-      this.gl.bindBuffer(target, null);
-    } else {
+    if (isIndexedBuffer) {
       this.gl.bindBufferBase(target, index, null);
+    } else {
+      this.gl.bindBuffer(target, null);
     }
     return this;
+  }
+
+  // gl.TRANSFORM_FEEDBACK_BUFFER_BINDING: Returns a WebGLBuffer.
+  // gl.TRANSFORM_FEEDBACK_BUFFER_SIZE: Returns a GLsizeiptr.
+  // gl.TRANSFORM_FEEDBACK_BUFFER_START: Returns a GLintptr.
+  // gl.UNIFORM_BUFFER_BINDING: Returns a WebGLBuffer.
+  // gl.UNIFORM_BUFFER_SIZE: Returns a GLsizeiptr.
+  // gl.UNIFORM_BUFFER_START: Returns a GLintptr.
+  getIndexedParameter(binding, index) {
+    // Create the buffer - if binding it here for the first time, this locks the type
+    // In WebGL2, use GL.COPY_READ_BUFFER to avoid locking the type
+    const target = this.gl.webgl2 ? GL.COPY_READ_BUFFER : this.target;
+    this.gl.bindBuffer(target, index);
+    return this.gl.getIndexedParameter(binding, index);
   }
 
   // PRIVATE METHODS
@@ -330,56 +306,11 @@ export default class Buffer extends Resource {
   }
 
   _getParameter(pname) {
-    this.bind();
+    this.gl.bindBuffer(this.target, this.handle);
     const value = this.gl.getBufferParameter(this.target, pname);
-    this.unbind();
+    this.gl.bindBuffer(this.target, null);
     return value;
   }
 }
 
-export class ElementArrayBuffer extends Buffer {
-  constructor(gl, opts) {
-    super(gl, Object.assign(opts, {target: GL.ELEMENT_ARRAY_BUFFER}));
-  }
-}
-
 Buffer.PARAMETERS = PARAMETERS;
-
-/*
-export class Attribute {
-  /**
-   * @classdesc
-   * Store characteristics of a data layout
-   * This data can be used when updating vertex attributes with
-   * the associated buffer, freeing the application from keeping
-   * track of this metadata.
-   *
-   * @class
-   * @param {GLuint} size - number of values per element (1-4)
-   * @param {GLuint} type - type of values (e.g. gl.FLOAT)
-   * @param {GLbool} normalized=false - normalize integers to [-1,1] or [0,1]
-   * @param {GLuint} integer=false - WebGL2 only, int-to-float conversion
-   * @param {GLuint} stride=0 - supports strided arrays
-   * @param {GLuint} offset=0 - supports strided arrays
-   *
-  constructor({
-    // Characteristics of stored data
-    buffer,
-    type,
-    size = 1,
-    offset = 0,
-    stride = 0,
-    normalized = false,
-    integer = false,
-    instanced = 0
-  } = {}) {
-    this.type = type;
-    this.size = size;
-    this.offset = offset;
-    this.stride = stride;
-    this.normalized = normalized;
-    this.integer = integer;
-    this.instanced = instanced;
-  }
-}
-  */
