ac51f78eb8f744179e2e6176e9c77b4a82436dfb
Shaojing Li <shaojing@uber.com>
2017-05-22 12:00:34 -0700

    Remove default mipmap generation

--- src/webgl/texture.js
+++ src/webgl/texture.js
@@ -122,7 +122,7 @@ export default class Texture extends Resource {
       format = GL.RGBA,
       type = GL.UNSIGNED_BYTE,
       border = 0,
-      mipmaps = true,
+      mipmaps = false,
       recreate = false,
       parameters = {},
       pixelStore = {}
