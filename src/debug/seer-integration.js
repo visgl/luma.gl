3c78c4b7bf1d261498b7121cb8532ae9e670f7d1
Balthazar Gronon <balthazar@uber.com>
2017-06-14 16:43:02 -0700

    refactor(dev): use latest seer api

--- src/debug/seer-integration.js
+++ src/debug/seer-integration.js
@@ -1,5 +1,7 @@
 import seer from 'seer';
 
+import {window} from '../utils/globals';
+
 const models = {};
 
 /**
@@ -10,6 +12,54 @@ export const addModel = model => {
     return;
   }
   models[model.id] = model;
+
+  seer.listItem('luma.gl', model.id);
+};
+
+const getType = obj => Object.prototype.toString.call(obj).slice(8, -1);
+
+const getAttribute = (key, attr) => {
+  const isArray = getType(attr).includes('Array');
+  if (isArray) {
+    return Array.prototype.slice.call(attr);
+  }
+  return attr;
+};
+
+/**
+ * Trim functions properties and convert typed arrays into simple ones
+ * to be able to pass the payload through postMessage.
+ */
+const transformPayload = payload => Object.keys(payload)
+  .reduce((acc, attrKey) => {
+
+    const attr = payload[attrKey];
+    acc[attrKey] = getType(attr) === 'Object' ? Object.keys(attr).reduce((out, key) => {
+      if (typeof attr[key] !== 'function') {
+        out[key] = getAttribute(key, attr[key]);
+      }
+      return out;
+    }, {}) : getAttribute(attrKey, attr);
+
+    return acc;
+
+  }, {});
+
+/**
+ * Log a model uniforms and attributes.
+ */
+export const logModel = (model, uniforms) => {
+  if (!window.__SEER_INITIALIZED__ || seer.throttle(`luma.gl:${model.id}`, 1E3)) {
+    return;
+  }
+
+  const attributesObject = Object.assign({}, model.geometry.attributes, model.attributes);
+  const uniformsObject = Object.assign({}, model.uniforms, uniforms);
+
+  seer.multiUpdate('luma.gl', model.id, [
+    {path: 'objects.uniforms', data: transformPayload(uniformsObject)},
+    {path: 'objects.attributes', data: transformPayload(attributesObject)}
+  ]);
 };
 
 /**
@@ -23,6 +73,10 @@ export const removeModel = id => {
  * Recursively traverse an object given a path of properties and set the given value
  */
 const recursiveSet = (obj, path, value) => {
+  if (!obj) {
+    return;
+  }
+
   if (path.length > 1) {
     recursiveSet(obj[path[0]], path.slice(1), value);
   } else {
@@ -30,16 +84,56 @@ const recursiveSet = (obj, path, value) => {
   }
 };
 
+const overrides = new Map();
+
+/**
+ * Create an override on the specify layer, indexed by a valuePath array.
+ * Do nothing in case Seer as not been initialized to prevent any preformance drawback.
+ */
+const setOverride = (id, valuePath, value) => {
+  if (!window.__SEER_INITIALIZED__) {
+    return;
+  }
+
+  if (!overrides.has(id)) {
+    overrides.set(id, new Map());
+  }
+
+  const uniforms = overrides.get(id);
+  uniforms.set(valuePath, value);
+};
+
+/**
+ * Apply overrides to a specific model's uniforms
+ */
+export const getOverrides = (id, uniforms) => {
+  if (!window.__SEER_INITIALIZED__ || !id) {
+    return;
+  }
+
+  const overs = overrides.get(id);
+  if (!overs) {
+    return;
+  }
+
+  overs.forEach((value, valuePath) => {
+    recursiveSet(uniforms, valuePath, value);
+  });
+};
+
 /**
  * Listen for luma.gl edit events
  */
 seer.listenFor('luma.gl', payload => {
   const model = models[payload.itemKey];
-  if (!model || payload.type !== 'edit') {
+  if (!model || payload.type !== 'edit' || payload.valuePath[0] !== 'uniforms') {
     return;
   }
 
+  const valuePath = payload.valuePath.slice(1);
+  setOverride(payload.itemKey, valuePath, payload.value);
+
   const uniforms = model.getUniforms();
-  recursiveSet(uniforms, payload.valuePath, payload.value);
+  recursiveSet(uniforms, valuePath, payload.value);
   model.setUniforms(uniforms);
 });
