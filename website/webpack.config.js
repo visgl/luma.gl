e2c931ca574f3850914d66decc643be04971274f
Ib Green <ibgreen@users.noreply.github.com>
2017-07-03 18:39:48 -0700

    Webpack configuration consolidation (#247)

---
+++ website/webpack.config.js
@@ -0,0 +1,113 @@
+const {resolve, join} = require('path');
+const webpack = require('webpack');
+
+const rootDir = join(__dirname, '..');
+const libSources = join(rootDir, 'src');
+
+// Otherwise modules imported from outside this directory does not compile
+// Seems to be a Babel bug
+// https://github.com/babel/babel-loader/issues/149#issuecomment-191991686
+const BABEL_CONFIG = {
+  presets: [
+    'es2015'
+  ].map(name => require.resolve(`babel-preset-${name}`)),
+  plugins: [
+    // '../babel-plugin-tree-shaking.js'
+    // math library subclasses arrays
+    [require.resolve('babel-plugin-transform-builtin-extend'), {globals: ['Array']}],
+    // website uses decorators
+    require.resolve('babel-plugin-transform-decorators-legacy')
+  ]
+};
+
+const COMMON_CONFIG = {
+  entry: ['./src/main'],
+
+  module: {
+    rules: [
+      {
+        // Compile ES2015 using babel
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: BABEL_CONFIG,
+        include: [resolve('..'), libSources],
+        exclude: [/node_modules/]
+      }, {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader', 'autoprefixer-loader']
+      }, {
+        test: /\.(eot|svg|ttf|woff|woff2|gif|jpe?g|png)$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    alias: {
+      'luma.gl': libSources
+    }
+  },
+
+  node: {
+    fs: 'empty'
+  },
+
+  plugins: []
+
+};
+
+const addDevConfig = config => {
+
+  config.module.rules.push({
+    // Unfortunately, webpack doesn't import library sourcemaps on its own...
+    test: /\.js$/,
+    use: ['source-map-loader'],
+    enforce: 'pre'
+  });
+
+  return Object.assign(config, {
+
+    devtool: 'source-maps',
+
+    plugins: config.plugins.concat([
+      // new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ])
+
+  });
+
+};
+
+const addProdConfig = config => {
+  // transpile mapbox-gl utils imported by react-map-gl@2
+  // these files are ES6 and break UglifyJS
+  config.module.rules.push({
+    loader: 'babel-loader'
+  });
+
+  module.exports = Object.assign(config, {
+    output: {
+      path: resolve(__dirname, '../dist'),
+      filename: 'bundle.js'
+    }
+  });
+};
+
+module.exports = env => {
+  env = env || {};
+
+  let config = COMMON_CONFIG;
+
+  if (env.local) {
+    config = addDevConfig(config);
+  }
+
+  if (env.prod) {
+    config = addProdConfig(config);
+  }
+
+  // Enable to debug config
+  // console.warn(JSON.stringify(config, null, 2));
+
+  return config;
+};
