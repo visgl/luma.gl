1970ce78017a3e5202af359c1b92b8d8a1f464d2
Ib Green <ib@uber.com>
2017-07-08 12:59:40 -0700

    Shader error parsing, shadertools export fix

--- test/webgl-utils/index.js
+++ test/webgl-utils/index.js
@@ -1,7 +1,7 @@
 // import './constants.spec';
-// import './format-glsl-error.spec';
 // import './get-error.spec';
-// import './get-shader-name.spec';
+import './get-shader-name.spec';
+import './format-glsl-error.spec';
 import './polyfill-context.spec';
 import './track-context-state.spec';
 import './set-parameters.spec';
