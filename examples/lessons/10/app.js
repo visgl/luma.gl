bd9cd0b2914a2f13f0434de7efba7cb9f4a5b590
Jian Huang <jianh@uber.com>
2017-09-08 15:19:34 -0700

    code changes based reviews: 1. use let than var, but after this change, example can't run on Safari anymore 2. new cameraInfo to capsulate several carema properties 3. fix tab/space issue 4. update luma.gl version to 4.0.0

--- examples/lessons/10/app.js
+++ examples/lessons/10/app.js
@@ -12,15 +12,17 @@ import {
 
 import {loadWorldGeometry, World} from './world';
 
-var pitch = 0;
-var pitchRate = 0;
-var yaw = 0;
-var yawRate = 0;
-var xPos = 0;
-var yPos = 0.4;
-var zPos = 0;
-var speed = 0;
-var direction = [0, 0, -1];
+const cameraInfo = {
+  pitch: 0,
+  pitchRate: 0,
+  yaw: 0,
+  yawRate: 0,
+  xPos: 0,
+  yPos: 0.4,
+  zPos: 0,
+  speed: 0,
+  direction: [0, 0, -1]
+};
 
 const animationLoop = new AnimationLoop({
   onInitialize: ({canvas, gl}) => {
@@ -36,31 +38,30 @@ const animationLoop = new AnimationLoop({
       urls: ['mud.gif']
     })
     .then(textures => {
-		  return loadFile({url: 'world.txt'})
-		  .then(file => {
-        var geometry = loadWorldGeometry(file);
-        var world = new World({
-        gl,
-        texture: textures[0],
-        geometry});
+      return loadFile({url: 'world.txt'})
+      .then(file => {
+        let geometry = loadWorldGeometry(file);
+        let world = new World({
+          gl,
+          texture: textures[0],
+          geometry});
         return {world};
-		  });
+      });
     });
   },
   onRender: ({
     gl, tick, aspect, world
   }) => {
     // Update Camera Position
-    const eyePos = [xPos, yPos, zPos];
+    const eyePos = [cameraInfo.xPos, cameraInfo.yPos, cameraInfo.zPos];
     const centerPos = new Matrix4()
-      .identity()
-      .rotateX(degToRad(pitch))
-      .rotateY(degToRad(yaw))
-      .transformVector3(direction)
+      .rotateX(degToRad(cameraInfo.pitch))
+      .rotateY(degToRad(cameraInfo.yaw))
+      .transformVector3(cameraInfo.direction)
       .add(eyePos);
     gl.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
 
-    var uMVMatrix = new Matrix4()
+    let uMVMatrix = new Matrix4()
       .lookAt({eye: eyePos, center: centerPos, up:[0, 1, 0]})
 
     world.render({
@@ -87,7 +88,7 @@ function degToRad(degree) {
   return degree / 180 * Math.PI;
 }
 
-var currentlyPressedKeys = {};
+let currentlyPressedKeys = {};
 
 function addKeyboardHandler(canvas) {
   addEvents(canvas, {
@@ -103,49 +104,49 @@ function addKeyboardHandler(canvas) {
 function handleKeys() {
   if (currentlyPressedKeys[33]) {
       // Page Up
-      pitchRate = 0.1;
+    cameraInfo.pitchRate = 0.1;
   } else if (currentlyPressedKeys[34]) {
       // Page Down
-      pitchRate = -0.1;
+    cameraInfo.pitchRate = -0.1;
   } else {
-      pitchRate = 0;
+    cameraInfo.pitchRate = 0;
   }
   if (currentlyPressedKeys[37] || currentlyPressedKeys[65]) {
       // Left cursor key or A
-      yawRate = 0.1;
+    cameraInfo.yawRate = 0.1;
   } else if (currentlyPressedKeys[39] || currentlyPressedKeys[68]) {
       // Right cursor key or D
-      yawRate = -0.1;
+    cameraInfo.yawRate = -0.1;
   } else {
-      yawRate = 0;
+    cameraInfo.yawRate = 0;
   }
   if (currentlyPressedKeys[38] || currentlyPressedKeys[87]) {
       // Up cursor key or W
-      speed = 0.003;
+    cameraInfo.speed = 0.003;
   } else if (currentlyPressedKeys[40] || currentlyPressedKeys[83]) {
       // Down cursor key
-      speed = -0.003;
+    cameraInfo.speed = -0.003;
   } else {
-      speed = 0;
+    cameraInfo.speed = 0;
   }
 }
 
-var lastTime = 0;
+let lastTime = 0;
 // Used to make us "jog" up and down as we move forward.
-var joggingAngle = 0;
+let joggingAngle = 0;
 
 function animate() {
-    var timeNow = new Date().getTime();
+    let timeNow = new Date().getTime();
     if (lastTime != 0) {
-        var elapsed = timeNow - lastTime;
-        if (speed != 0) {
-            xPos -= Math.sin(degToRad(yaw)) * speed * elapsed;
-            zPos -= Math.cos(degToRad(yaw)) * speed * elapsed;
-            joggingAngle += elapsed * 0.6; // 0.6 "fiddle factor" - makes it feel more realistic :-)
-            yPos = Math.sin(degToRad(joggingAngle)) / 20 + 0.4
+        let elapsed = timeNow - lastTime;
+        if (cameraInfo.speed != 0) {
+          cameraInfo.xPos -= Math.sin(degToRad(cameraInfo.yaw)) * cameraInfo.speed * elapsed;
+          cameraInfo.zPos -= Math.cos(degToRad(cameraInfo.yaw)) * cameraInfo.speed * elapsed;
+          cameraInfo.joggingAngle += elapsed * 0.6; // 0.6 "fiddle factor" - makes it feel more realistic :-)
+          cameraInfo.yPos = Math.sin(degToRad(joggingAngle)) / 20 + 0.4
         }
-        yaw += yawRate * elapsed;
-        pitch += pitchRate * elapsed;
+        cameraInfo.yaw += cameraInfo.yawRate * elapsed;
+        cameraInfo.pitch += cameraInfo.pitchRate * elapsed;
     }
     lastTime = timeNow;
 }
@@ -154,4 +155,3 @@ export default animationLoop;
 
 // expose on Window for standalone example
 window.animationLoop = animationLoop; // eslint-disable-lie
-
--- examples/lessons/10/world.js
+++ examples/lessons/10/world.js
@@ -30,11 +30,11 @@ void main(void) {
 `;
 
 function loadWorldGeometry(data) {
-  var lines = data.split("\n");
-  var vertexPositions = [];
-  var vertexTextureCoords = [];
-  for (var i in lines) {
-	var vals = lines[i].replace(/^\s+/, "").split(/\s+/);
+  let lines = data.split("\n");
+  let vertexPositions = [];
+  let vertexTextureCoords = [];
+  for (let i in lines) {
+	let vals = lines[i].replace(/^\s+/, "").split(/\s+/);
     if (vals.length == 5 && vals[0] != "//") {
       // It is a line describing a vertex; get X, Y and Z first
       vertexPositions.push(parseFloat(vals[0]));
