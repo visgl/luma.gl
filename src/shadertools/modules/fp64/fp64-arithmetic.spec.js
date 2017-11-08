e98b6d45cd3ec837d4d5830aa6e039f90447b9f8
Ib Green <ibgreen@users.noreply.github.com>
2017-11-08 12:31:28 -0800

    Make fp64 tests work under `tape` (#351)

--- src/shadertools/modules/fp64/fp64-arithmetic.spec.js
+++ src/shadertools/modules/fp64/fp64-arithmetic.spec.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2015 - 2017 Uber Technologies, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
+// of this software and associated documentation files (the 'Software'), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
@@ -10,7 +10,7 @@
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -18,29 +18,27 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-export {fp64ify} from './fp64-utils';
+// Actual tests for different arithmetic functions
 
-import fp64arithmeticShader from './fp64-arithmetic.glsl';
-import fp64functionShader from './fp64-functions.glsl';
+import test from 'tape-catch';
+import {testcase, gl} from './fp64-test-utils';
 
-const fp64shader = `${fp64arithmeticShader}\n${fp64functionShader}`;
+test('fp64#sum_fp64', t => {
+  testcase(gl, {glslFunc: 'sum_fp64', binary: true, op: (a, b) => a + b, t});
+});
 
-export default {
-  name: 'fp64',
-  vs: fp64shader,
-  fs: null
-};
+test('fp64#sub_fp64', t => {
+  testcase(gl, {glslFunc: 'sub_fp64', binary: true, op: (a, b) => a - b, t});
+});
 
-// Arithmetic only
-export const fp64arithmetic = {
-  name: 'fp64-arithmetic',
-  vs: `${fp64arithmeticShader}`,
-  fs: null
-};
+test('fp64#mul_fp64', t => {
+  testcase(gl, {glslFunc: 'mul_fp64', binary: true, op: (a, b) => a * b, limit: 128, t});
+});
 
-// Fragment shader fp64
-export const fp64fs = {
-  name: 'fp64-fs',
-  vs: null,
-  fs: fp64shader
-};
+test('fp64#div_fp64', t => {
+  testcase(gl, {glslFunc: 'div_fp64', binary: true, op: (a, b) => a / b, limit: 128, t});
+});
+
+test('fp64#sqrt_fp64', t => {
+  testcase(gl, {glslFunc: 'sqrt_fp64', op: (a) => Math.sqrt(a), limit: 128, t});
+});
