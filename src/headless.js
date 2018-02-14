57095f2b7c796cd4b56f02f2c58b1d20b8dc581b
Ib Green <ib@uber.com>
2018-02-14 05:02:29 -0800

    wip

--- src/headless.js
+++ src/headless.js
@@ -1,16 +1,28 @@
+/* eslint-disable quotes, no-console */
+/* global console */
 import luma from './init';
 import isBrowser from './utils/is-browser';
+import assert from 'assert';
+
+const ERR_NO_HEADLESS_GL = `\
+Failed to dynamically load headless gl. \
+gl not installed or not accessible from this directory.`;
 
 if (!isBrowser) {
-  luma.globals.headlessGL = require('gl');
-  luma.globals.headlessTypes = require('gl/wrap');
-  if (!luma.globals.headlessTypes.WebGLRenderingContext) {
-    throw new Error('Could not access headless WebGL type definitions');
+  try {
+    luma.globals.headlessGL = module.require('gl');
+    luma.globals.headlessTypes = module.require('gl/wrap');
+  } catch (error) {
+    console.error(ERR_NO_HEADLESS_GL);
+    console.error(`Node error: ${error.message}`);
   }
+  assert(luma.globals.headlessTypes.WebGLRenderingContext);
 }
 
 // Make sure luma.gl initializes with valid types
 require('./webgl/api/types');
 
-// Now import standard luma.gl package
-// module.exports = require('./index');
+export const isWebglAvailable = isBrowser || luma.globals.headlessGL;
+
+// Create context
+export default luma.globals.headlessGL;
