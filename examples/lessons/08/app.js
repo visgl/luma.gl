dc75cfba129c678fdf4e8cf8f32a2b8c6f390899
Xintong Xia <veronica.sha@gmail.com>
2018-03-12 23:04:09 +0100

    fix example 08 with controls (#440)

--- examples/lessons/08/app.js
+++ examples/lessons/08/app.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-var */
 /* eslint-disable max-statements, array-bracket-spacing, no-multi-spaces */
 import {
   GL, AnimationLoop, Cube, Matrix4,
-  addEvents, loadTextures, resetParameters, setParameters
+  addEvents, loadTextures, setParameters
 } from 'luma.gl';
 
 // Vertex shader with lighting
@@ -59,11 +58,11 @@ void main(void) {
 }
 `;
 
-var xRot = 0;
-var xSpeed = 0.01;
-var yRot = 0;
-var ySpeed = 0.0;
-var cubePositionZ = -5.0;
+let xRot = 0;
+let xSpeed = 0.01;
+let yRot = 0;
+let ySpeed = 0.0;
+let cubePositionZ = -5.0;
 
 const animationLoop = new AnimationLoop({
   onInitialize: ({canvas, gl}) => {
@@ -84,17 +83,17 @@ const animationLoop = new AnimationLoop({
         [gl.TEXTURE_MAG_FILTER]: gl.LINEAR
       }]
     })
-    .then(textures => ({
-      cube: new Cube(gl, {
-        vs: VERTEX_SHADER,
-        fs: FRAGMENT_SHADER,
-        uniforms: {uSampler: textures[0]}
-      })
-    }));
+      .then(textures => ({
+        cube: new Cube(gl, {
+          vs: VERTEX_SHADER,
+          fs: FRAGMENT_SHADER,
+          uniforms: {uSampler: textures[0]}
+        })
+      }));
   },
   onRender: ({
-    gl, tick, aspect, cube
-  }) => {
+               gl, tick, aspect, cube
+             }) => {
     xRot += xSpeed;
     yRot += ySpeed;
 
@@ -111,47 +110,53 @@ const animationLoop = new AnimationLoop({
       .rotateXYZ([tick * 0.01, tick * 0.01, tick * 0.01])
       .multiplyRight(cube.matrix);
 
+
+    const {
+      blending,
+      lighting,
+      ambient,
+      direction
+    } = getControls();
+
+    if (blending.checked) {
+      gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+      gl.enable(gl.BLEND);
+      gl.disable(gl.DEPTH_TEST);
+      cube.setUniforms({
+        'alpha': Number(0.5)
+      });
+    } else {
+      gl.disable(gl.BLEND);
+      gl.enable(gl.DEPTH_TEST);
+    }
+
+    // Update scene config with light info
+    const useLight = lighting.checked;
+    const ambientColor = [
+      parseValue(ambient.r.value),
+      parseValue(ambient.g.value),
+      parseValue(ambient.b.value)
+    ];
+    const lightingDirection = [
+      parseValue(direction.x.value),
+      parseValue(direction.y.value),
+      parseValue(direction.z.value)
+    ];
+    const directionalColor = [
+      parseValue(direction.r.value),
+      parseValue(direction.g.value),
+      parseValue(direction.b.value)
+    ];
+
     cube.render({
       uMVMatrix,
       uPMatrix: new Matrix4().perspective({aspect}),
-      uAmbientColor: [0.2, 0.2, 0.2],
-      uLightingDirection: [0, 0, 1],
-      uDirectionalColor: [0.8, 0.8, 0.8],
-      uUseLighting: true,
+      uAmbientColor: ambientColor, //[0.2, 0.2, 0.2],
+      uLightingDirection: lightingDirection, //[0, 0, 1],
+      uDirectionalColor: directionalColor, //[0.8, 0.8, 0.8],
+      uUseLighting: useLight,
       uAlpha: Number(0.5)
     });
-
-    // TODO: using static values for lighting/blending uniforms, use this code
-    // once below 'getControls()' is enabled
-    //  if (blending.checked) {
-    //     gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
-    //     gl.enable(gl.BLEND);
-    //     gl.disable(gl.DEPTH_TEST);
-    //     cube.setUniforms({
-    //       'alpha': Number(0.5)
-    //     });
-    //   } else {
-    //     gl.disable(gl.BLEND);
-    //     gl.enable(gl.DEPTH_TEST);
-    //   }
-    //   Update scene config with light info
-    //   var lightConfig = scene.config.lights;
-    //   lightConfig.enable = lighting.checked;
-    //   lightConfig.ambient = {
-    //     r: Number(ambient.r.value),
-    //     g: Number(ambient.g.value),
-    //     b: Number(ambient.b.value)
-    //   };
-    //   lightConfig.directional.direction = {
-    //     x: Number(direction.x.value),
-    //     y: Number(direction.y.value),
-    //     z: Number(direction.z.value)
-    //   };
-    //   lightConfig.directional.color = {
-    //     r: Number(direction.r.value),
-    //     g: Number(direction.g.value),
-    //     b: Number(direction.b.value)
-    //   };
   }
 });
 
@@ -170,56 +175,68 @@ function addKeyboardHandler(canvas) {
   addEvents(canvas, {
     onKeyDown(e) {
       switch (e.key) {
-      case 'up':
-        xSpeed -= 0.02;
-        break;
-      case 'down':
-        xSpeed += 0.02;
-        break;
-      case 'left':
-        ySpeed -= 0.02;
-        break;
-      case 'right':
-        ySpeed += 0.02;
-        break;
-      // handle page up/down
-      default:
-        if (e.code === 33) {
-          cubePositionZ -= 0.05;
-        } else if (e.code === 34) {
-          cubePositionZ += 0.05;
-        }
+        case 'up':
+          xSpeed -= 0.02;
+          break;
+        case 'down':
+          xSpeed += 0.02;
+          break;
+        case 'left':
+          ySpeed -= 0.02;
+          break;
+        case 'right':
+          ySpeed += 0.02;
+          break;
+        // handle page up/down
+        default:
+          if (e.code === 33) {
+            cubePositionZ -= 0.05;
+          } else if (e.code === 34) {
+            cubePositionZ += 0.05;
+          }
       }
     }
   });
 }
 
-// TODO: these controls don't show on webpage, fix it then enable this code.
-// function getControls() {
-//   // Lighting form elements variables
-//   var $id = function(d) {
-//     return document.getElementById(d);
-//   };
-//
-//   // Get lighting form elements
-//   var lighting = $id('lighting');
-//   var ambient = {
-//     r: $id('ambientR'),
-//     g: $id('ambientG'),
-//     b: $id('ambientB')
-//   };
-//   var direction = {
-//     x: $id('lightDirectionX'),
-//     y: $id('lightDirectionY'),
-//     z: $id('lightDirectionZ'),
-//
-//     r: $id('directionalR'),
-//     g: $id('directionalG'),
-//     b: $id('directionalB')
-//   };
-//   var blending = $id('blending');
-//   var alpha = $id('alpha');
-// }
+function parseValue(value) {
+  let parsed = Number(value);
+  return isNaN(parsed) ? 0 : parsed;
+}
+
+function getControls() {
+  // Lighting form elements variables
+  const $id = function (d) {
+    return document.getElementById(d);
+  };
+
+  // Get lighting form elements
+  const lighting = document.getElementById('lighting');
+  const ambient = {
+    r: $id('ambientR'),
+    g: $id('ambientG'),
+    b: $id('ambientB')
+  };
+  const direction = {
+    x: $id('lightDirectionX'),
+    y: $id('lightDirectionY'),
+    z: $id('lightDirectionZ'),
+
+    r: $id('directionalR'),
+    g: $id('directionalG'),
+    b: $id('directionalB')
+  };
+  const blending = $id('blending');
+  const alpha = $id('alpha');
+
+  return {
+    lighting,
+    ambient,
+    direction,
+    blending,
+    alpha
+  }
+}
 
 export default animationLoop;
 
