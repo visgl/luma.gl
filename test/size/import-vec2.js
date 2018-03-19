53002a22a5e8b3eb50c680e9d0e6d207ce3294fa
Ib Green <ib@uber.com>
2018-03-19 11:12:27 -0700

    Remove unused size tests

--- test/size/import-vec2.js
+++
@@ -1,3 +0,0 @@
-import {Vector2} from 'math.gl';
-
-console.log(Vector2); // eslint-disable-line
--- test/size/import-vec4-mat4-explicit.js
+++
@@ -1,5 +0,0 @@
-import {Vector4} from 'math.gl/vector4';
-import {Matrix4} from 'math.gl/matrix4';
-
-console.log(Vector4); // eslint-disable-line
-console.log(Matrix4); // eslint-disable-line
