2c248f1ce7685467c006a498018da7179ad235de
Ib Green <ib@uber.com>
2018-03-21 10:40:18 -0700

    Add esnext dist and main field for big bundle size savings

--- src/utils/is-old-ie.js
+++ src/utils/is-old-ie.js
@@ -1,8 +1,9 @@
 import {window} from './globals';
 
-export default function isOldIE() {
+// opts allows user agent to be overridden for testing
+export default function isOldIE(opts = {}) {
   const navigator = window.navigator || {};
-  const userAgent = navigator.userAgent || '';
+  const userAgent = opts.userAgent || navigator.userAgent;
   // We only care about older versions of IE (IE 11 and below). Newer versions of IE (Edge)
   // have much better web standards support.
   const isMSIE = userAgent.indexOf('MSIE ') !== -1;
