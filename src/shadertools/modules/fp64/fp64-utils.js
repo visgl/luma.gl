633886bce18f3e0fb52d6cb0e94442fe8c09a687
Xiaoji Chen <xiaoji@uber.com>
2018-05-19 16:58:08 -0700

    port fp64ify utils from deck.gl

--- src/shadertools/modules/fp64/fp64-utils.js
+++ src/shadertools/modules/fp64/fp64-utils.js
@@ -1,5 +1,24 @@
-export function fp64ify(a) {
+export function fp64ify(a, array = [], startIndex = 0) {
   const hiPart = Math.fround(a);
-  const loPart = a - hiPart;
-  return [hiPart, loPart];
+  const loPart = a - Math.fround(a);
+  array[startIndex] = hiPart;
+  array[startIndex + 1] = loPart;
+  return array;
+}
+
+export function fp64LowPart(a) {
+  return a - Math.fround(a);
+}
+
+// calculate WebGL 64 bit matrix (transposed "Float64Array")
+export function fp64ifyMatrix4(matrix) {
+  // Transpose the projection matrix to column major for GLSL.
+  const matrixFP64 = new Float32Array(32);
+  for (let i = 0; i < 4; ++i) {
+    for (let j = 0; j < 4; ++j) {
+      const index = i * 4 + j;
+      fp64ify(matrix[j * 4 + i], matrixFP64, index * 2);
+    }
+  }
+  return matrixFP64;
 }
