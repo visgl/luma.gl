669a6e963019d7469e30357bee07f8f6e7eb9f0a
Xiaoji Chen <xiaoji@uber.com>
2018-05-19 17:15:45 -0700

    add jsdoc

--- src/shadertools/modules/fp64/fp64-utils.js
+++ src/shadertools/modules/fp64/fp64-utils.js
@@ -1,16 +1,32 @@
-export function fp64ify(a, array = [], startIndex = 0) {
+/**
+ * Calculate WebGL 64 bit float
+ * @param a {number} - the input float number
+ * @param out {array, optional} - the output array. If not supplied, a new array is created.
+ * @param startIndex {integer, optional} - the index in the output array to fill from. Default 0.
+ * @returns {array} - the fp64 representation of the input number
+ */
+export function fp64ify(a, out = [], startIndex = 0) {
   const hiPart = Math.fround(a);
-  const loPart = a - Math.fround(a);
-  array[startIndex] = hiPart;
-  array[startIndex + 1] = loPart;
-  return array;
+  const loPart = a - hiPart;
+  out[startIndex] = hiPart;
+  out[startIndex + 1] = loPart;
+  return out;
 }
 
+/**
+ * Calculate the low part of a WebGL 64 bit float
+ * @param a {number} - the input float number
+ * @returns {number} - the lower 32 bit of the number
+ */
 export function fp64LowPart(a) {
   return a - Math.fround(a);
 }
 
-// calculate WebGL 64 bit matrix (transposed "Float64Array")
+/**
+ * Calculate WebGL 64 bit matrix (transposed "Float64Array")
+ * @param matrix {Matrix4} - the input matrix
+ * @returns {array} - the fp64 representation of the input matrix
+ */
 export function fp64ifyMatrix4(matrix) {
   // Transpose the projection matrix to column major for GLSL.
   const matrixFP64 = new Float32Array(32);
