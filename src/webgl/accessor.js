9b82733f85557f1840dd6fe73eb6fc502a94ea42
Ib Green <ib@uber.com>
2018-06-12 08:03:39 -0700

    Add Accessor and ProgramConfiguration helper classes

---
+++ src/webgl/accessor.js
@@ -0,0 +1,78 @@
+const GL_FLOAT = 0x1406;
+
+export default class Accessor {
+
+  static get defaultOptions() {
+    return {
+      type: GL_FLOAT,
+      size: 1,
+      offset: 0,
+      stride: 0,
+      normalized: false,
+      integer: false,
+      instanced: 0
+    };
+  }
+
+  /**
+   * Store characteristics of a data accessor
+   * This data can be used when updating vertex attributes with the associated buffer,
+   * freeing the application from keeping track of this metadata.
+   * @class
+   * {type, size, offset, stride, normalized, integer, instanced}
+   * @param {GLuint} size - number of values per element (1-4)
+   * @param {GLuint} type - type of values (e.g. gl.FLOAT)
+   * @param {GLbool} normalized=false - normalize integers to [-1,1] or [0,1]
+   * @param {GLuint} integer=false - WebGL2 only, int-to-float conversion
+   * @param {GLuint} stride=0 - supports strided arrays
+   * @param {GLuint} offset=0 - supports strided arrays
+   */
+  constructor(...optsList) {
+    optsList.forEach(opts => this._update(opts));
+  }
+
+  // Combine with other accessors
+  getOptions(...optsList) {
+    const combinedOpts = Object.assign({}, Accessor.defaultOptions, this);
+    optsList.forEach(opts => this._update(opts, combinedOpts));
+    return combinedOpts;
+  }
+
+  update(opts) {
+    this._update(opts);
+    return this;
+  }
+
+  _update(opts = {}, target = this) {
+    if (opts.type !== undefined) {
+      target.type = opts.type;
+    }
+    if (opts.size !== undefined) {
+      target.size = opts.size;
+    }
+    if (opts.offset !== undefined) {
+      target.offset = opts.offset;
+    }
+    if (opts.stride !== undefined) {
+      target.stride = opts.stride;
+    }
+    if (opts.normalized !== undefined) {
+      target.normalized = opts.normalized;
+    }
+    if (opts.integer !== undefined) {
+      target.integer = opts.integer;
+    }
+    if (opts.divisor !== undefined) {
+      target.divisor = opts.divisor;
+    }
+
+    // Backwards compatibility
+    if (opts.instanced !== undefined) {
+      target.divisor = opts.instanced ? 1 : 0;
+    }
+    if (opts.isInstanced !== undefined) {
+      target.divisor = opts.isInstanced ? 1 : 0;
+    }
+    return target;
+  }
+}
---
+++ src/webgl/program-configuration.js
@@ -0,0 +1,59 @@
+// Contains metadata describing attribute configurations for a program's shaders
+// Much of this is automatically extracted from shaders after program linking
+import {decomposeCompositeGLType} from '../webgl-utils/attribute-utils';
+import Accessor from './accessor';
+import assert from '../utils/assert';
+
+export default class ProgramConfiguration {
+
+  constructor(program, varyingMap = {}) {
+    this.attributeInfos = [];
+    this.attributeInfosByName = {};
+    this.varyingMap = varyingMap;
+    Object.seal(this);
+    this._readAttributesFromProgram(program);
+  }
+
+  getAttributeInfo(locationOrName) {
+    const location = Number(locationOrName);
+    if (Number.isFinite(location)) {
+      return this.attributeInfos[location];
+    }
+    return this.attributeInfosByName[locationOrName] || null;
+  }
+
+  getLocation(locationOrName) {
+    const attributeInfo = this.getAttributeInfo();
+    return attributeInfo ? attributeInfo.location : -1;
+  }
+
+  // linkProgram needs to have been called, although linking does not need to have been successful
+  _readAttributesFromProgram(program) {
+    const {gl} = program;
+    const count = gl.getProgramParameter(program.handle, gl.ACTIVE_ATTRIBUTES);
+
+    for (let location = 0; location < count; location++) {
+      const {name, type, size} = gl.getActiveAttrib(program.handle, location);
+      this._addAttribute(location, name, type, size);
+    }
+  }
+
+  _addAttribute(location, name, compositeType, size) {
+    const {type, components} = decomposeCompositeGLType(compositeType);
+    const accessor = new Accessor({type, size: size * components});
+    this._inferProperties(name, accessor);
+
+    const attributeInfo = {location, name, accessor}; // Base values
+    this.attributeInfos.push(attributeInfo);
+    this.attributeInfosByName[attributeInfo.name] = attributeInfo; // For quick name based lookup
+  }
+
+  // Extract additional attribute metadata from shader names (based on attribute naming conventions)
+  _inferProperties(location, name, accessor) {
+    if ((/instance/i).test(name)) {
+      // Any attribute containing the word "instance" will be assumed to be instanced
+      accessor.update({instanced: true});
+    }
+  }
+}
+
