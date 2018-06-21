b6df369e818e19b9902f852bbccfbcbb05c8b2e7
Ib Green <ib@uber.com>
2018-06-21 15:57:35 -0700

    Remove unused tests

--- test/index-webgl-dependent-tests.js
+++ test/index-webgl-dependent-tests.js
@@ -1,7 +1,6 @@
 // Imports tests for all modules that depend on webgl
 
-import '../src/shadertools/test';
-import './src/shadertools';
+import '../src/shadertools/test'; // Shadertool tests are placed within submodule
 import './src/webgl1';
 import './src/webgl-utils';
 import './src/webgl-context';
