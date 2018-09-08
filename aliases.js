3978636febd08cab8b496d064ab0866594d87a2c
Ib Green <ibgreen@users.noreply.github.com>
2018-09-07 18:43:38 -0700

    glfx: image processing shaders packaged as luma.gl modules, plus example (#717)

--- aliases.js
+++ aliases.js
@@ -24,6 +24,7 @@ const ALIASES = {
   'luma.gl/test': path.resolve(__dirname, './test'),
   'luma.gl': path.resolve(__dirname, './modules/core/src'),
   'luma.gl-imageprocessing': path.resolve(__dirname, './modules/imageprocessing/src'),
+  'luma.gl-glfx': path.resolve(__dirname, './modules/glfx/src'),
   'luma.gl-io': path.resolve(__dirname, './modules/io/src'),
   'loaders.gl': path.resolve(__dirname, './modules/loaders.gl/src')
 };
---
+++ modules/glfx/src/warp-filters/warp.js
@@ -0,0 +1,19 @@
+// TODO: FIXME
+/* eslint-disable */
+const fs = `\
+vec4 warp_sampleColor(sampler2D texture, vec2 texSize, vec2 coord) {
+  vec4 color = texture2D(texture, coord / texSize);
+  vec2 clampedCoord = clamp(coord, vec2(0.0), texSize);
+  if (coord != clampedCoord) {
+    /* fade to transparent if we are outside the image */
+    color.a *= max(0.0, 1.0 - length(coord - clampedCoord));
+  }
+
+  return color;
+}
+`;
+
+export default {
+  name: 'warp',
+  fs
+};
