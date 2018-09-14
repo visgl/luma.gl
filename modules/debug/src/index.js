d8184da617c349b5a94eb5a65500513f351240aa
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2018-09-13 20:35:44 -0400

    Add debug example (#726)

--- modules/debug/src/index.js
+++ modules/debug/src/index.js
@@ -1,4 +1,4 @@
 export {COLOR_MODE} from './draw-model';
 
-export {default as DebugContext} from './debug-context';
+export {default as _DebugContext} from './debug-context';
 export {compileVertexShader, compileFragmentShader} from './compile-shader';
