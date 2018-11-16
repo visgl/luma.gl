7bf43659041ec75efb38f9b692319c002cd9f964
Xiaoji Chen <xiaoji@uber.com>
2018-11-16 12:04:27 -0800

    fix cubemap example in worker

--- examples/wip/worker/worker.js
+++ examples/wip/worker/worker.js
@@ -1,10 +1,10 @@
 import {_AnimationLoopProxy as AnimationLoopProxy} from 'luma.gl';
 
-// import animationLoop from '../../core/cubemap/app';  // not working - accesses document
+import animationLoop from '../../core/cubemap/app';
 // import animationLoop from '../../core/fragment/app';
 // import animationLoop from '../../core/instancing/app';
 // import animationLoop from '../../core/mandelbrot/app';
-import animationLoop from '../../core/picking/app';
+// import animationLoop from '../../core/picking/app';
 // import animationLoop from '../../core/shadowmap/app';
 // import animationLoop from '../../core/transform/app';
 // import animationLoop from '../../core/transform-feedback/app';
