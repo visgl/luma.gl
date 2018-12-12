f18bf7ca1403deea956e7f15e2a301ddec872076
wentsul <wenzel@uber.com>
2018-12-12 14:29:10 -0500

    Update polyfillContext extension initialization to non-iterable returned from getSupportedExtensions

--- test/modules/core/webgl1/polyfill-context.spec.js
+++ test/modules/core/webgl1/polyfill-context.spec.js
@@ -1,5 +1,6 @@
 import polyfillContext from 'luma.gl/webgl1/polyfill-context';
 import test from 'tape-catch';
+import {makeSpy} from 'probe.gl/test-utils';
 
 import {fixture} from 'luma.gl/test/setup';
 
@@ -15,6 +16,27 @@ test('WebGL#polyfillContext', t => {
     const extensions2 = polyfillContext(gl2);
     t.ok(extensions2, 'extensions were returned');
   }
+
+  t.end();
+});
+
+
+test('WebGL#polyfillContext getSupportedExtensions when context is lost', t => {
+  const {gl, gl2} = fixture;
+
+  const getSupportedExtensionsSpy = makeSpy(gl, 'getSupportedExtensions');
+  getSupportedExtensionsSpy.returns(null);
+
+  const extensions = polyfillContext(gl);
+  t.ok(extensions, 'extensions were returned');
+
+  if (gl2) {
+    const getSupportedExtensions2Spy = makeSpy(gl2, 'getSupportedExtensions');
+    getSupportedExtensions2Spy.returns(null);
+    const extensions2 = polyfillContext(gl2);
+    t.ok(extensions2, 'extensions were returned');
+  }
+
 
   t.end();
 });
