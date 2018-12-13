7140b8c8bb16873fff046f89dcba3face7c6947b
wentsul <wenzel@uber.com>
2018-12-13 08:48:46 -0500

    retore stub after test completion

--- test/modules/core/webgl1/polyfill-context.spec.js
+++ test/modules/core/webgl1/polyfill-context.spec.js
@@ -29,6 +29,7 @@ test('WebGL#polyfillContext getSupportedExtensions when context is lost', t => {
 
   const extensions = polyfillContext(gl);
   t.ok(extensions, 'extensions were returned');
+  getSupportedExtensionsSpy.restore();
 
   if (gl2) {
     const getSupportedExtensions2Spy = makeSpy(gl2, 'getSupportedExtensions');
