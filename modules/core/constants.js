a1a24f1d595e76142f6c9e71e65bfc37b6da3111
Ib Green <ibgreen@users.noreply.github.com>
2019-01-08 15:52:50 -0800

    Fix deprecated constant export (#840)

--- modules/core/constants.js
+++ modules/core/constants.js
@@ -1,3 +1,5 @@
 // eslint-disable-next-line
-console.warn('"import `luma.gl/constants`"" has been replaced with "import `@luma.gl/constants`"');
-require('@luma.gl/constants');
+console.warn(`import 'luma.gl/constants' has been replaced with '@luma.gl/constants'
+ (scoped npm package) and will be removed in a future version`);
+
+module.exports = require('@luma.gl/constants');
--- test/index-webgl-independent-tests.js
+++ test/index-webgl-independent-tests.js
@@ -1,5 +1,8 @@
 // Imports tests for all modules that do not depend on WebGL
 
 import './dev-modules';
+
+import './modules/constants';
+
 import './modules/core/utils';
 import './modules/core/geometry';
