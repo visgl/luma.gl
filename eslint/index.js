1a2148e87463a7bbd53bcb6c7beb8efeb44db755
Georgios Karnas <georgios@uber.com>
2019-01-30 18:08:39 -0800

    create custom eslint rule to detect wrong probe.gl logger usage

---
+++ eslint/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  rules: {
+    "check-log-call": {
+      create: function(context) {
+        return {
+          CallExpression(node) {
+            if (node.callee && node.callee.object && node.callee.object.name === 'log' && node.parent && node.parent.type !== 'CallExpression') {
+              context.report(node, 'Use log.' + node.callee.property.name + '(...)()');
+            }
+          }
+        };
+      }
+    }
+  }
+};
+
