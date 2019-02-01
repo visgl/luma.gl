50bf5f1b2b2b96f14246a52d3301f8d8c31b7146
Georgios Karnas <georgios@uber.com>
2019-01-31 19:17:03 -0800

    Fix `Accessor.type` to override buffer typed array

--- modules/core/src/webgl/classes/vertex-array.js
+++ modules/core/src/webgl/classes/vertex-array.js
@@ -30,6 +30,7 @@ export default class VertexArray {
 
     // Extracted information
     this.elements = null;
+    this.elementsAccessor = null;
     this.values = null;
     this.accessors = null;
     this.unused = null;
@@ -65,6 +66,7 @@ export default class VertexArray {
     // this.vertexArrayObject.reset();
 
     this.elements = null;
+    this.elementsAccessor = null;
     const {MAX_ATTRIBUTES} = this.vertexArrayObject;
     this.values = new Array(MAX_ATTRIBUTES).fill(null);
     this.accessors = new Array(MAX_ATTRIBUTES).fill(null);
@@ -132,6 +134,7 @@ export default class VertexArray {
   // Must be a Buffer bound to GL.ELEMENT_ARRAY_BUFFER. Constants not supported
   setElementBuffer(elementBuffer = null, accessor = {}) {
     this.elements = elementBuffer; // Save value for debugging
+    this.elementsAccessor = accessor;
     this.clearDrawParams();
 
     // Update vertexArray immediately if we have our own array
@@ -145,7 +148,7 @@ export default class VertexArray {
   setBuffer(locationOrName, buffer, appAccessor = {}) {
     // Check target
     if (buffer.target === GL.ELEMENT_ARRAY_BUFFER) {
-      return this.setElementBuffer(buffer);
+      return this.setElementBuffer(buffer, appAccessor);
     }
 
     const {location, accessor} = this._resolveLocationAndAccessor(
@@ -387,7 +390,7 @@ export default class VertexArray {
       // index type is saved for drawElement calls
       drawParams.elementCount = this.elements.getElementCount(this.elements.accessor);
       drawParams.isIndexed = true;
-      drawParams.indexType = this.elements.accessor.type;
+      drawParams.indexType = this.elementsAccessor.type || this.elements.accessor.type;
     }
 
     // Post-calculation checks
