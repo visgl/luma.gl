ed211b9ec099228358a5620e3af4958d6ed1288f
Ib Green <ib@uber.com>
2019-02-04 14:31:49 -0800

    Improve tests

--- test/modules/constants/constants.spec.js
+++ test/modules/constants/constants.spec.js
@@ -6,10 +6,3 @@ test('@luma.gl/constants', t => {
   t.equal(typeof GL, 'object', '@luma.gl/constants is an object');
   t.end();
 });
-
-test('luma.gl/constants (DEPRECATED)', t => {
-  t.comment('Intentionally triggering deprecation warning!');
-  const GL_DEPRECATED = require('luma.gl/constants');
-  t.equal(GL_DEPRECATED, GL, 'luma.gl/constants still correctly importing constants');
-  t.end();
-});
