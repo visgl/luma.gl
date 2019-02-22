176f10f6f4f81e5d7c0d5924bbcc7ab9762a0a15
Taylor Baldwin <tbaldwin@uber.com>
2019-02-22 16:41:01 -0500

    add tests for animation loop start/stop functionality

--- test/modules/core/core/animation-loop.spec.js
+++ test/modules/core/core/animation-loop.spec.js
@@ -1,4 +1,4 @@
-/* global document */
+/* global document, setTimeout */
 import {AnimationLoop} from 'luma.gl';
 import test from 'tape-catch';
 import {fixture} from 'luma.gl/test/setup';
@@ -70,4 +70,79 @@ test('WebGL#AnimationLoop redraw', t => {
       renderCalled++;
     }
   }).start();
+});
+
+test('WebGL#AnimationLoop two start()s should only run one loop', t => {
+  if (typeof document === 'undefined') {
+    t.comment('browser-only test');
+    t.end();
+    return;
+  }
+
+  const {gl} = fixture;
+  let initializeCalled = 0;
+
+  const animationLoop = new AnimationLoop({
+    gl,
+    onInitialize: () => {
+      initializeCalled++;
+    },
+    onRender: () => {
+      animationLoop.stop();
+      // FIXME: this is not directly test that only one loop has been started
+      t.is(initializeCalled, 1, 'onInitialize called');
+      t.end();
+    }
+  });
+  animationLoop.start();
+  animationLoop.start();
+});
+
+test('WebGL#AnimationLoop start followed immediately by stop() should stop', t => {
+  if (typeof document === 'undefined') {
+    t.comment('browser-only test');
+    t.end();
+    return;
+  }
+
+  const {gl} = fixture;
+  let initializeCalled = 0;
+
+  const animationLoop = new AnimationLoop({
+    gl,
+    onInitialize: () => {
+      initializeCalled++;
+    }
+  });
+  animationLoop.start();
+  animationLoop.stop();
+  setTimeout(() => {
+    t.is(initializeCalled, 0, 'onInitialize called');
+    t.end();
+  }, 100);
+});
+
+test('WebGL#AnimationLoop a start/stop/start should not call initialize again', t => {
+  if (typeof document === 'undefined') {
+    t.comment('browser-only test');
+    t.end();
+    return;
+  }
+
+  const {gl} = fixture;
+  let initializeCalled = 0;
+
+  const animationLoop = new AnimationLoop({
+    gl,
+    onInitialize: () => {
+      initializeCalled++;
+    }
+  });
+  animationLoop.start();
+  setTimeout(() => animationLoop.stop(), 50);
+  setTimeout(() => animationLoop.start(), 100);
+  setTimeout(() => {
+    t.is(initializeCalled, 1, 'onInitialize called');
+    t.end();
+  }, 150);
 });
