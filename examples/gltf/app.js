a6a441eacd5bb6cce0c4ba39503cbd8749f88a54
Georgios Karnas <georgios@uber.com>
2019-03-01 15:15:01 -0800

    Convert PBR to use Standard Light parameters, Part 2

--- examples/gltf/app.js
+++ examples/gltf/app.js
@@ -225,7 +225,7 @@ export class DemoApp {
       model.updateModuleSettings({
         lightSources: {
           directionalLights: [{
-              color: [1.0, 1.0, 1.0],
+              color: [255, 255, 255],
               direction: [0.0, 0.5, 0.5],
               intensity: 1.0,
             }
@@ -236,7 +236,7 @@ export class DemoApp {
       model.updateModuleSettings({
         lightSources: {
           directionalLights: [{
-              color: [1.0, 1.0, 1.0],
+              color: [255, 255, 255],
               direction: [1.0, 0.0, 0.0],
               intensity: 1.0,
             }
@@ -247,15 +247,15 @@ export class DemoApp {
       model.updateModuleSettings({
         lightSources: {
           directionalLights: [{
-              color: [1.0, 0.0, 0.0],
+              color: [255, 0.0, 0.0],
               direction: [1.0, 0.0, 0.0],
               intensity: 1.0,
             },{
-              color: [0.0, 0.0, 1.0],
+              color: [0.0, 0.0, 255],
               direction: [0.0, 0.0, 1.0],
               intensity: 1.0,
             },{
-              color: [0.0, 1.0, 0.0],
+              color: [0.0, 255, 0.0],
               direction: [0.0, 1.0, 0.0],
               intensity: 1.0,
             }
