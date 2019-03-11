4ecc6146948a68529dea614edda9e871b7fb0ccc
Ib Green <ib@uber.com>
2019-03-11 14:20:06 -0700

    wip

--- modules/core/src/webgl/features/features.js
+++ modules/core/src/webgl/features/features.js
@@ -7,56 +7,57 @@ import WEBGL_FEATURES from './webgl-features-table';
 import {isWebGL2} from '../utils';
 import {assert} from '../../utils';
 
-// Create a key-mirrored FEATURES array
-// TODO - refactor, this has tree-shaking side effects
-const FEATURES = {};
-Object.keys(WEBGL_FEATURES).forEach(key => {
-  FEATURES[key] = key;
-});
-export {FEATURES};
-
-// TODO - cache the value
-function getFeature(gl, cap) {
-  const feature = WEBGL_FEATURES[cap];
-  assert(feature, cap);
-
-  // Get extension name from table
-  const extensionName = isWebGL2(gl) ? feature[1] || feature[0] : feature[0];
-
-  // Check if the value is dependent on checking an extension
-  const value =
-    typeof extensionName === 'string' ? Boolean(gl.getExtension(extensionName)) : extensionName;
-
-  assert(value === false || value === true);
-
-  return value;
-}
-
-// capability can be a WebGL extension name or a luma capability name
+// Check one feature
 export function hasFeature(gl, feature) {
   return hasFeatures(gl, feature);
 }
 
+// Check one or more features
 export function hasFeatures(gl, features) {
   features = Array.isArray(features) ? features : [features];
   return features.every(feature => {
-    return getFeature(gl, feature);
+    return isFeatureSupported(gl, feature);
   });
 }
 
+// Return a list of supported features
 export function getFeatures(gl) {
   gl.luma = gl.luma || {};
-
   if (!gl.luma.caps) {
     gl.luma.caps = {};
     gl.luma.caps.webgl2 = isWebGL2(gl);
     for (const cap in WEBGL_FEATURES) {
-      gl.luma.caps[cap] = getFeature(gl, cap);
+      gl.luma.caps[cap] = isFeatureSupported(gl, cap);
     }
   }
   return gl.luma.caps;
 }
 
-export const TEST_EXPORTS = {
-  WEBGL_FEATURES
-};
+// TODO - cache the value
+function isFeatureSupported(gl, cap) {
+  const feature = WEBGL_FEATURES[cap];
+  assert(feature, cap);
+
+  // Get extension name from table
+  const featureDefinition = isWebGL2(gl) ? feature[1] || feature[0] : feature[0];
+
+  let isSupported;
+
+  // Check if the value is dependent on checking one or more extensions
+  if (typeof featureDefinition === 'function') {
+    isSupported = featureDefinition(gl);
+  } else if (Array.isArray(featureDefinition)) {
+    isSupported = true;
+    for (const extension of featureDefinition) {
+      isSupported = isSupported && Boolean(gl.getExtension(extension));
+    }
+  } else if (typeof featureDefinition === 'string') {
+    isSupported = Boolean(gl.getExtension(featureDefinition));
+  } else if (typeof featureDefinition === 'boolean') {
+    isSupported = featureDefinition;
+  } else {
+    assert(false);
+  }
+
+  return isSupported;
+}
