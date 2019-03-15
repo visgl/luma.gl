3f2034b5b9dce4c3ea7eab4eb21942920ba2fa52
Ib Green <ib@uber.com>
2019-03-15 13:10:47 -0700

    More tests

--- modules/shadertools/src/modules/lights/lights.spec.js
+++ modules/shadertools/src/modules/lights/lights.spec.js
@@ -5,14 +5,14 @@ test('shadertools#lights', t => {
   let uniforms = lights.getUniforms();
   t.ok(uniforms, 'Generated default uniforms');
 
-  uniforms = lights.getUniforms({lights: [
-  	{type: 'ambient'}, {type: 'directional'}, {type: 'point'}
-  ]});
+  uniforms = lights.getUniforms({
+    lights: [{type: 'ambient'}, {type: 'directional'}, {type: 'point'}]
+  });
   t.ok(uniforms, 'Generated uniforms for empty lights');
 
-  uniforms = lights.getUniforms({lights: [
-  	{type: 'non-existing'}
-  ]});
+  uniforms = lights.getUniforms({
+    lights: [{type: 'non-existing'}]
+  });
   t.ok(uniforms, 'Generated uniforms for non-supported light object');
 
   t.end();
