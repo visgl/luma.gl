b2a5a882289ebd31ac9db406177dfaf78178bd84
Ib Green <ibgreen@users.noreply.github.com>
2019-03-25 20:30:15 -0700

    Add some test plumbing for coverage (#986)

--- modules/constants/test/constants.spec.js
+++ modules/constants/test/constants.spec.js
@@ -1,8 +1,19 @@
 import test from 'tape-catch';
+import {fixture} from 'test/setup';
 
 import GL from '@luma.gl/constants';
 
 test('@luma.gl/constants', t => {
   t.equal(typeof GL, 'object', '@luma.gl/constants is an object');
+  t.end();
+});
+
+test('@luma.gl/constants#WebGL1 context', t => {
+  for (const key in fixture.gl) {
+    const value = fixture.gl[key];
+    if (Number.isFinite(value) && key.toUpperCase() === key && GL[key] !== undefined) {
+      t.equals(GL[key], value, `GL.${key} is equal to gl.${key}`);
+    }
+  }
   t.end();
 });
--- test/webgl-dependent-tests.js
+++ test/webgl-dependent-tests.js
@@ -4,17 +4,21 @@
 import '@luma.gl/debug';
 
 // Generic helper modules
+import '../modules/constants/test';
 import '../modules/webgl2-polyfill/test';
 import '../modules/webgl-state-tracker/test';
+import '../modules/debug/test/';
 
 // luma.gl core module: Test webgl-dependent code now
 import '../modules/webgl/test';
 import '../modules/core/test/webgl-dependent-tests';
 
+import '../modules/addons/test/';
+
 // luma.gl sub modules
-import '../modules/debug/test/';
 import '../modules/gpgpu/test/';
 import '../modules/effects/test';
+import '../modules/glfx/test/';
 
 // The classic "unscoped" luma.gl module (Supported for backwards compatibility)
 import '../modules/main//test';
