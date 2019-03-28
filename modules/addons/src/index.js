5e07ee2952c59fcd6160f9f243297565d199e961
Georgios Karnas <georgios@uber.com>
2019-03-27 20:45:40 -0700

    Create GLTFScenegraphLoader and change example to use it

--- modules/addons/src/index.js
+++ modules/addons/src/index.js
@@ -4,6 +4,7 @@ export {default as VRAnimationLoop} from './webvr/vr-animation-loop';
 // glTF Scenegraph Instantiator
 export {default as createGLTFObjects} from './gltf/create-gltf-objects';
 export {default as GLTFEnvironment} from './gltf/gltf-environment';
+export {GLBScenegraphLoader, GLTFScenegraphLoader} from './gltf/gltf-scenegraph-loader';
 
 // Misc
 export {addEvents} from './events/add-events';
