ff814e789e48cae4a17f956cd39f151aaf156f00
Georgios Karnas <georgios@uber.com>
2019-04-02 16:25:42 -0700

    glTF Loader: Allow to async return after all resources are loaded

--- modules/addons/src/gltf/gltf-scenegraph-loader.js
+++ modules/addons/src/gltf/gltf-scenegraph-loader.js
@@ -1,6 +1,13 @@
+/* global window */
 import {GLTFParser} from '@loaders.gl/gltf';
 import {DracoDecoder} from '@loaders.gl/draco';
 import createGLTFObjects from './create-gltf-objects';
+
+async function waitWhileCondition(condition) {
+  while (condition()) {
+    await new Promise(resolve => window.requestAnimationFrame(resolve));
+  }
+}
 
 async function parse(data, options, uri, loader) {
   const gltfParser = new GLTFParser();
@@ -10,7 +17,25 @@ async function parse(data, options, uri, loader) {
     DracoDecoder
   });
 
-  return Object.assign({gltfParser, gltf}, createGLTFObjects(options.gl, gltf, options));
+  const gltfObjects = createGLTFObjects(options.gl, gltf, options);
+
+  if (options.waitForFullLoad) {
+    const remaining = [];
+
+    gltfObjects.scenes.forEach(scene => {
+      scene.traverse(model => {
+        Object.values(model.model.program.uniforms).forEach(uniform => {
+          if (uniform.loaded === false) {
+            remaining.push(uniform);
+          }
+        });
+      });
+    });
+
+    await waitWhileCondition(() => remaining.some(uniform => !uniform.loaded));
+  }
+
+  return Object.assign({gltfParser, gltf}, gltfObjects);
 }
 
 export const GLBScenegraphLoader = {
