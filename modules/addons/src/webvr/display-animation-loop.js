bddb4f9f1dba3b44059708f1d33dc1266bdb7895
Ib Green <ib@uber.com>
2019-04-08 07:59:55 -0700

    Make DisplayAnimationLoop smaller

--- modules/addons/src/webvr/display-animation-loop.js
+++ modules/addons/src/webvr/display-animation-loop.js
@@ -1,60 +1,30 @@
-import {AnimationLoop, withParameters} from '@luma.gl/core';
+import {AnimationLoop} from '@luma.gl/core';
 
 // This code could be folded into the core animation loop
 export default class DisplayAnimationLoop extends AnimationLoop {
-  setDisplay(display) {
-    if (this.display !== display) {
-      if (this.display) {
-        this.display.detachDisplay();
-      }
-
-      this.display = display;
-
-      if (this.display && this.gl) {
-        this.display.attachDisplay(this.gl);
-      }
-    }
+  finalize() {
+    this._setDisplay(null);
   }
 
-  onFrame(...args) {
-    const views = this.display && this.display.getViews();
-    if (!views) {
-      return super.onFrame(...args);
+  _setDisplay(display) {
+    // store anumation loop on the display
+    if (display) {
+      display.animationLoop = this;
     }
 
-    const options = args[0];
-    const {width, height} = options;
-
-    // Need both vrPresenting and vrFrame
-    // to avoid race conditions when we exit VR
-    // after we schedule an animation frame
-    for (const view of views) {
-      const [x, y, w, h] = view.viewport;
-      const viewRect = [x * width, y * height, w * width, h * height];
-
-      withParameters(
-        this.gl,
-        {
-          viewport: viewRect,
-          scissor: viewRect,
-          scissorTest: true
-        },
-        () => super.onRender({...options, ...view})
-      );
+    if (!display && this.display) {
+      this.display.finalize();
+      this.display.animationLoop = null;
     }
 
-    this.display.submitFrame();
-    this.gl.viewport(0, 0, width, height);
-    return null;
+    this.display = display;
   }
 
-  _createWebGLContext(...args) {
-    const before = this.gl;
-    super._createWebGLContext(...args);
-
-    if (before !== this.gl && this.display) {
-      this.display.attachDisplay(this.gl);
+  _renderFrame(...args) {
+    if (this.display) {
+      return this.display._renderFrame();
     }
+    return super.onFrame(...args);
   }
 
   _requestAnimationFrame(renderFrame) {
--- modules/addons/src/webvr/display.js
+++ modules/addons/src/webvr/display.js
@@ -1,3 +1,5 @@
+import {withParameters} from '@luma.gl/core';
+
 export default class Display {
   attachDisplay(gl) {
     this.gl = gl;
@@ -29,5 +31,39 @@ export default class Display {
   // return true if animation frame has been requested
   requestAnimationFrame() {
     return false;
+  }
+
+  // AnimationLoop calls this API
+
+  _renderFrame(...args) {
+    const views = this.getViews();
+    if (!views) {
+      return false;
+    }
+
+    const options = args[0];
+    const {width, height} = options;
+
+    // Need both vrPresenting and vrFrame
+    // to avoid race conditions when we exit VR
+    // after we schedule an animation frame
+    for (const view of views) {
+      const [x, y, w, h] = view.viewport;
+      const viewRect = [x * width, y * height, w * width, h * height];
+
+      withParameters(
+        this.gl,
+        {
+          viewport: viewRect,
+          scissor: viewRect,
+          scissorTest: true
+        },
+        () => this.animationLoop.onRender({...options, ...view})
+      );
+    }
+
+    this.submitFrame();
+    this.gl.viewport(0, 0, width, height);
+    return true;
   }
 }
