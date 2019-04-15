0243f6bc7bd2208a0a7c3aced10ad0ecffb1dd54
Tarek Sherif <tsherif@uber.com>
2019-04-15 18:06:39 -0400

    Add gltf license info

--- examples/core/gltf/app.js
+++ examples/core/gltf/app.js
@@ -17,6 +17,9 @@ const CUBE_FACE_TO_DIRECTION = {
   [GL.TEXTURE_CUBE_MAP_NEGATIVE_Z]: 'back'
 };
 
+// Damaged helmet model used under creative commons: https://github.com/KhronosGroup/glTF-Sample-Models/tree/1ba47770292486e66ca1e1161857a6e5695c2631/2.0/DamagedHelmet
+// Papermill textures used under Apache 2.0: https://github.com/KhronosGroup/glTF-Sample-Viewer/blob/e2d487693fa2e6148bd29d05bc82586f5a002a45/LICENSE.md
+
 const RAW_GITHUB = 'https://raw.githubusercontent.com/uber/luma.gl/master/examples/core/gltf/';
 const GLTF_BASE_URL = `${RAW_GITHUB}assets/`;
 
