8d29b240d9cfa8cfeb2704a518576a2e7fc40537
Georgios Karnas <georgios@uber.com>
2019-04-17 23:43:24 -0700

    improve gltf-animator

--- modules/addons/src/gltf/gltf-animator.js
+++ modules/addons/src/gltf/gltf-animator.js
@@ -1,4 +1,5 @@
-import {Matrix4} from 'math.gl';
+import {assert, log} from '@luma.gl/core';
+import {Matrix4, Quaternion} from 'math.gl';
 
 // TODO: import from loaders.gl?
 export const ATTRIBUTE_TYPE_TO_COMPONENTS = {
@@ -64,6 +65,103 @@ function applyTranslationRotationScale(gltfNode, node) {
   }
 }
 
+const quaternion = new Quaternion();
+function linearInterpolate(target, path, start, stop, ratio) {
+  if (path === 'rotation') {
+    // SLERP when path is rotation
+    quaternion.slerp({start, target: stop, ratio});
+    for (let i = 0; i < quaternion.length; i++) {
+      target[path][i] = quaternion[i];
+    }
+  } else {
+    // regular interpolation
+    for (let i = 0; i < start.length; i++) {
+      target[path][i] = ratio * stop[i] + (1 - ratio) * start[i];
+    }
+  }
+}
+
+function cubicsplineInterpolate(target, path, {p0, outTangent0, inTangent1, p1, tDiff, ratio: t}) {
+  // TODO: Quaternion might need normalization
+  for (let i = 0; i < target[path].length; i++) {
+    const m0 = outTangent0[i] * tDiff;
+    const m1 = inTangent1[i] * tDiff;
+    target[path][i] =
+      (2 * Math.pow(t, 3) - 3 * Math.pow(t, 2) + 1) * p0[i] +
+      (Math.pow(t, 3) - 2 * Math.pow(t, 2) + t) * m0 +
+      (-2 * Math.pow(t, 3) + 3 * Math.pow(t, 2)) * p1[i] +
+      (Math.pow(t, 3) - Math.pow(t, 2)) * m1;
+  }
+}
+
+function stepInterpolate(target, path, value) {
+  for (let i = 0; i < value.length; i++) {
+    target[path][i] = value[i];
+  }
+}
+
+function interpolate(time, {input, interpolation, output}, target, path) {
+  const maxTime = input[input.length - 1];
+  const animationTime = time % maxTime;
+
+  const nextIndex = input.findIndex(t => t >= animationTime);
+  const previousIndex = Math.max(0, nextIndex - 1);
+
+  if (!Array.isArray(target[path])) {
+    switch (path) {
+      case 'translation':
+        target[path] = [0, 0, 0];
+        break;
+
+      case 'rotation':
+        target[path] = [0, 0, 0, 1];
+        break;
+
+      case 'scale':
+        target[path] = [1, 1, 1];
+        break;
+
+      default:
+        log.warn(`Bad animation path ${path}`)();
+    }
+  }
+
+  assert(target[path].length === output[previousIndex].length);
+  const previousTime = input[previousIndex];
+  const nextTime = input[nextIndex];
+
+  switch (interpolation) {
+    case 'STEP':
+      stepInterpolate(target, path, output[previousIndex]);
+      break;
+
+    case 'LINEAR':
+      if (nextTime > previousTime) {
+        const ratio = (animationTime - previousTime) / (nextTime - previousTime);
+        linearInterpolate(target, path, output[previousIndex], output[nextIndex], ratio);
+      }
+      break;
+
+    case 'CUBICSPLINE':
+      if (nextTime > previousTime) {
+        const ratio = (animationTime - previousTime) / (nextTime - previousTime);
+        const tDiff = nextTime - previousTime;
+
+        const p0 = output[3 * previousIndex + 1];
+        const outTangent0 = output[3 * previousIndex + 2];
+        const inTangent1 = output[3 * nextIndex + 0];
+        const p1 = output[3 * nextIndex + 1];
+
+        cubicsplineInterpolate(target, path, {p0, outTangent0, inTangent1, p1, tDiff, ratio});
+      }
+      break;
+
+    default:
+      log.warn(`Interpolation ${interpolation} not supported`)();
+      break;
+  }
+}
+
 class GLTFAnimation {
   constructor(props) {
     this.startTime = 0;
@@ -81,14 +179,8 @@ class GLTFAnimation {
     const absTime = timeMs / 1000;
     const time = (absTime - this.startTime) * this.speed;
 
-    this.channels.forEach(({sampler: {input, output}, target, path}) => {
-      // TODO: support "interpolation"
-      let index = input.findIndex(t => t > time);
-      if (index === -1) {
-        index = 0;
-        this.startTime = absTime;
-      }
-      target[path] = output[index];
+    this.channels.forEach(({sampler, target, path}) => {
+      interpolate(time, sampler, target, path);
       applyTranslationRotationScale(target, target._node);
     });
   }
