3beca9986edc31eba58a557a745345af72d25a7b
Tarek Sherif <tareksherif@pm.me>
2019-04-19 12:48:42 -0400

    TextEncoder polyfill for gltf example (#1078)

--- examples/core/gltf/app.js
+++ examples/core/gltf/app.js
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 /* global document, window */
+import './encoding.js';
 import {parse} from '@loaders.gl/core';
 // eslint-disable-next-line import/no-unresolved
 import {DracoLoader} from '@loaders.gl/draco';
