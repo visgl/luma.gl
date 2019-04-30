dd065a47c173d10effbb8185323c3bc590d8b597
Ib Green <ib@uber.com>
2019-04-30 08:05:37 -0700

    wip

---
+++ website-gatsby/templates/lessons/example-02.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AnimationLoopRunner from '../../src/components/animation-loop-runner';
+import AnimationLoop from '../../examples/lessons/02/app';
+
+export default class Example extends React.Component {
+  render() {
+    return (
+      <AnimationLoopRunner AnimationLoop={AnimationLoop} />
+    );
+  }
+}
