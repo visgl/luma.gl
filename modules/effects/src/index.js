2ea4716257a785627fb3ece3852046b77617cfa9
Jian Huang <jianh@uber.com>
2019-05-16 14:20:17 -0700

    move shader modules from glfx to effects

--- modules/effects/src/index.js
+++ modules/effects/src/index.js
@@ -1,5 +1,34 @@
 // Shader modules
 
+// glfx ADJUST shader modules
+export {default as brightnessContrast} from './modules/adjust-filters/brightnesscontrast';
+export {default as denoise} from './modules/adjust-filters/denoise';
+export {default as hueSaturation} from './modules/adjust-filters/huesaturation';
+export {default as noise} from './modules/adjust-filters/noise';
+export {default as sepia} from './modules/adjust-filters/sepia';
+export {default as unsharpMask} from './modules/adjust-filters/unsharpmask';
+export {default as vibrance} from './modules/adjust-filters/vibrance';
+export {default as vignette} from './modules/adjust-filters/vignette';
+
+export {default as curves} from './modules/curve-filters/curves';
+
+// glfx BLUR shader modules
+export {default as lensBlur} from './modules/blur-filters/lensblur';
+export {default as tiltShift} from './modules/blur-filters/tiltshift';
+export {default as triangleBlur} from './modules/blur-filters/triangleblur';
+export {default as zoomBlur} from './modules/blur-filters/zoomblur';
+
+// glfx FUN shader modules
+export {default as colorHalftone} from './modules/fun-filters/colorhalftone';
+export {default as dotScreen} from './modules/fun-filters/dotscreen';
+export {default as edgeWork} from './modules/fun-filters/edgework';
+export {default as hexagonalPixelate} from './modules/fun-filters/hexagonalpixelate';
+export {default as ink} from './modules/fun-filters/ink';
+
+// glfx WARP shader modules
+export {default as bulgePinch} from './modules/warp-filters/bulgepinch';
+export {default as swirl} from './modules/warp-filters/swirl';
+
 // experimental shader modules and passes
 export {default as _depth} from './experimental/modules/depth';
 export {default as _ConvolutionPass} from './experimental/passes/convolution-pass';
