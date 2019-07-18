d8abe1f1214c817c7406ede00cc32e06e8b8fc8a
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-07-17 23:32:21 -0700

    Update coverage configs (#1165)

--- modules/glfx/bundle.js
+++ modules/glfx/bundle.js
@@ -1,5 +1,5 @@
 /* global window, global */
-const moduleExports = require('./index');
+const moduleExports = require('./src');
 
 const _global = typeof window === 'undefined' ? global : window;
 _global.loaders = _global.luma || {};
--- modules/webgl2-polyfill/src/bundle.js
+++
@@ -1,7 +0,0 @@
-/* global window, global */
-const moduleExports = require('./index');
-
-const _global = typeof window === 'undefined' ? global : window;
-_global.loaders = _global.luma || {};
-
-module.exports = Object.assign(_global.luma, moduleExports);
