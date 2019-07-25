3091c15fcb7c900cef2d079d22fc573ab81289c2
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-07-25 16:25:41 -0700

    remove addEvents from addons (#1180)

--- modules/addons/test/add-events.spec.js
+++ modules/addons/test/add-events.spec.js
@@ -2,6 +2,6 @@ import test from 'tape-catch';
 import {addEvents} from '@luma.gl/addons';
 
 test('@luma.gl/addons#addEvents', t => {
-  t.equal(typeof addEvents, 'function', 'addEvents is an object');
+  t.throws(() => addEvents(), 'addEvents is removed');
   t.end();
 });
