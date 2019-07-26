3e7643c98e1a2fc0e7eb866622406b5a701c3f89
Xiaoji Chen <xiaoji@uber.com>
2019-07-26 16:37:15 -0700

    remove diffImagePixels from gpgpu

--- modules/gpgpu/src/index.js
+++ modules/gpgpu/src/index.js
@@ -1,7 +1,5 @@
 // GPGPU utilities for luma.gl
 
-export {diffImagePixels} from './diff-images';
-
 export {
   buildHistopyramidBaseLevel as _buildHistopyramidBaseLevel,
   getHistoPyramid as _getHistoPyramid,
