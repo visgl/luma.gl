8b3a0a6e15500ff77319f5da66582cb58177bfa1
Ib Green <ib@uber.com>
2019-08-27 15:05:41 -0700

    Address comments

--- modules/webgl/src/debug/debug-vertex-array.js
+++ modules/webgl/src/debug/debug-vertex-array.js
@@ -42,8 +42,14 @@ export function getDebugTableForVertexArray({vertexArray, header = 'Attributes'}
 
 /* eslint-disable max-statements */
 function getDebugTableRow(vertexArray, attribute, accessor, header) {
-  // const round = xnum => Math.round(num * 10) / 10;
   const {gl} = vertexArray;
+
+  if (!attribute) {
+    return {
+      [header]: 'null',
+      'Format ': 'N/A'
+    };
+  }
 
   let type = 'NOT PROVIDED';
   let size = 'N/A';
@@ -63,13 +69,6 @@ function getDebugTableRow(vertexArray, attribute, accessor, header) {
 
     // Look for 'nt' to detect integer types, e.g. Int32Array, Uint32Array
     isInteger = type.indexOf('nt') !== -1;
-  }
-
-  if (!attribute) {
-    return {
-      [header]: 'null',
-      'Format ': 'N/A'
-    };
   }
 
   if (attribute instanceof Buffer) {
@@ -117,8 +116,5 @@ function getDebugTableRow(vertexArray, attribute, accessor, header) {
 function getGLSLDeclaration(name, accessor) {
   const {type, size} = accessor;
   const typeAndName = getCompositeGLType(type, size);
-  if (typeAndName) {
-    return `${name} (${typeAndName.name})`;
-  }
-  return name;
+  return typeAndName ? `${name} (${typeAndName.name})` : name;
 }
