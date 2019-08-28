2b94e23bae1fc19ee1ee6b188cf67386cf626eab
Tarek Sherif <tsherif@uber.com>
2019-08-28 19:05:58 -0400

    Fix double program release in base model (#1223)

--- modules/core/src/lib/base-model.js
+++ modules/core/src/lib/base-model.js
@@ -306,12 +306,8 @@ export default class BaseModel {
       return;
     }
 
-    if (this.program) {
-      if (this.programManager) {
-        this.programManager.release(this.program);
-      } else {
-        this.program.delete();
-      }
+    if (this.program && !this.programManager) {
+      this.program.delete();
     }
 
     this.program = program;
