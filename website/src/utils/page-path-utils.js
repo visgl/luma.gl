db0a0e3733078205215b02aaed059d736e987e1c
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-09-05 13:09:54 -0700

    point docs to release branch (#1229)

--- website/src/utils/page-path-utils.js
+++ website/src/utils/page-path-utils.js
@@ -1,11 +1,3 @@
-function getDocUrl(filename) {
-  let url = `docs/${filename}`;
-  if (filename.indexOf('markdown') !== -1) {
-    url = filename;
-  }
-  return url;
-}
-
 // mapping from file path in source to generated page url
 export const markdownFiles = {};
 
@@ -14,15 +6,15 @@ export function generatePaths(tree, {docUrls = false, parentPath = ''} = {}) {
     tree.forEach(branch => generatePaths(branch, {docUrls, parentPath}));
   }
   if (tree.name) {
-    tree.path = tree.name.match(
-      /(3D|API|WebGL|GLB|PLY|LAS|PCD|OBJ|KML|GLTF|([A-Z]|^)[a-z]+|\d+)/g).join('-'
-    ).toLowerCase();
+    tree.path = tree.name
+      .match(/(3D|API|WebGL|GLB|PLY|LAS|PCD|OBJ|KML|GLTF|([A-Z]|^)[a-z]+|\d+)/g)
+      .join('-')
+      .toLowerCase();
   }
   if (tree.children) {
     generatePaths(tree.children, {docUrls, parentPath: `${parentPath}/${tree.path}`});
   }
   if (docUrls && typeof tree.content === 'string') {
-    tree.content = getDocUrl(tree.content);
     const i = tree.content.indexOf('docs/');
     if (i >= 0) {
       markdownFiles[tree.content.slice(i)] = `${parentPath}/${tree.path}`;
