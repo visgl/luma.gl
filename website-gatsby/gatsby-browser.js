1b7cb7b05f7974da4f33b24020c7695adba7d64e
Travis Gorkin <tgorkin@uber.com>
2019-09-18 11:22:18 -0700

    Fix some styling and layout on gatsby website example info panels. (#1248)

--- website-gatsby/gatsby-browser.js
+++ website-gatsby/gatsby-browser.js
@@ -1,13 +1,2 @@
-// const {registerReactComponent} = require('ocular-gatsby/api');
-
-// const Hero = require('./src/components/hero').default;
-// const ExampleRunner = require('./src/components/example-runner').default;
-// const EXAMPLES = require('./src/components/examples');
-
+// eslint-disable-next-line import/no-extraneous-dependencies
 module.exports = require('ocular-gatsby/gatsby-browser');
-
-// Make sure to regsiter custom components after ocular registers defaults
-
-// registerReactComponent('Hero', Hero);
-// registerReactComponent('ExampleRunner', ExampleRunner);
-// registerReactComponent('EXAMPLES', EXAMPLES);
