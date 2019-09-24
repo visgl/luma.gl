f1f0047fefaff59b9ffd35606e430f9b618b4339
1chandu <chandu@uber.com>
2019-09-24 16:18:00 -0700

    Update checking support for rendering to float textures (#1257)
    * Update checking support for rendering to float textures

--- modules/webgl/src/features/webgl-features-table.js
+++ modules/webgl/src/features/webgl-features-table.js
@@ -1,3 +1,5 @@
+import Framebuffer from '../classes/framebuffer';
+import Texture2D from '../classes/texture-2d';
 // TODO - this should be the default export, test cases need updating
 export const FEATURES = {
   WEBGL2: 'WEBGL2',
@@ -39,6 +41,34 @@ export const FEATURES = {
   GLSL_TEXTURE_LOD: 'GLSL_TEXTURE_LOD'
 };
 
+// TODO: remove this global (https://github.com/uber/luma.gl/issues/1258)
+let float32ColorAttachmentSupport = null;
+
+// function to test if Float 32 bit format texture can be bound as color attachment
+function checkFloat32ColorAttachment(gl) {
+  // if previously queried, return the cache value.
+  if (float32ColorAttachmentSupport !== null) {
+    return float32ColorAttachmentSupport;
+  }
+  const testTexture = new Texture2D(gl, {
+    format: gl.RGBA,
+    type: gl.FLOAT,
+    dataFormat: gl.RGBA
+  });
+  const testFb = new Framebuffer(gl, {
+    id: `test-framebuffer`,
+    check: false,
+    attachments: {
+      [gl.COLOR_ATTACHMENT0]: testTexture
+    }
+  });
+  const status = testFb.getStatus();
+  testTexture.delete();
+  testFb.delete();
+  float32ColorAttachmentSupport = status === gl.FRAMEBUFFER_COMPLETE;
+  return float32ColorAttachmentSupport;
+}
+
 // Defines luma.gl "feature" names and semantics
 // Format: 'feature-name: [WebGL1 support, WebGL2 support] / [WebGL1 and WebGL2 support]', when support is 'string' it is the name of the extension
 export default {
@@ -70,7 +100,7 @@ export default {
   [FEATURES.TEXTURE_FILTER_ANISOTROPIC]: ['EXT_texture_filter_anisotropic'],
 
   // FRAMEBUFFERS, TEXTURES AND RENDERBUFFERS
-  [FEATURES.COLOR_ATTACHMENT_RGBA32F]: ['WEBGL_color_buffer_float', 'EXT_color_buffer_float'],
+  [FEATURES.COLOR_ATTACHMENT_RGBA32F]: [checkFloat32ColorAttachment, 'EXT_color_buffer_float'],
   [FEATURES.COLOR_ATTACHMENT_FLOAT]: [false, 'EXT_color_buffer_float'],
   [FEATURES.COLOR_ATTACHMENT_HALF_FLOAT]: ['EXT_color_buffer_half_float'],
 
