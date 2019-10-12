9f70b12c5ab99e5a38f532f42cdcbc1b8807224b
Tarek Sherif <tsherif@uber.com>
2019-10-11 20:22:38 -0400

    Miscelaneous cleanup

--- modules/core/src/scenegraph/index.js
+++ modules/core/src/scenegraph/index.js
@@ -3,4 +3,3 @@
 export {default as ScenegraphNode} from './nodes/scenegraph-node';
 export {default as GroupNode} from './nodes/group-node';
 export {default as ModelNode} from './nodes/model-node';
-export {default as CameraNode} from './nodes/camera-node';
