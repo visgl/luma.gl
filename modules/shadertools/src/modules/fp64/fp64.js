3247ef497ac50eb74327b1b02e5c6cf329f235de
Xiaoji Chen <Pessimistress@users.noreply.github.com>
2019-12-06 14:26:26 -0800

    Export fp64-arithmetic shader module (#1309)

--- modules/shadertools/src/modules/fp64/fp64.js
+++ modules/shadertools/src/modules/fp64/fp64.js
@@ -23,7 +23,6 @@ import {fp64ify, fp64LowPart, fp64ifyMatrix4} from './fp64-utils';
 import fp64arithmeticShader from './fp64-arithmetic.glsl';
 import fp64functionShader from './fp64-functions.glsl';
 
-const fp64shader = `${fp64arithmeticShader}\n${fp64functionShader}`;
 const CONST_UNIFORMS = {
   // Used in LUMA_FP64_CODE_ELIMINATION_WORKAROUND
   ONE: 1.0
@@ -31,29 +30,26 @@ const CONST_UNIFORMS = {
 export {fp64ify, fp64LowPart, fp64ifyMatrix4};
 
 function getUniforms() {
-  return Object.assign({}, CONST_UNIFORMS);
+  return CONST_UNIFORMS;
 }
-
-export default {
-  name: 'fp64',
-  vs: fp64shader,
-  fs: null,
-  fp64ify,
-  fp64LowPart,
-  fp64ifyMatrix4,
-  getUniforms
-};
 
 // Arithmetic only
 export const fp64arithmetic = {
   name: 'fp64-arithmetic',
-  vs: `${fp64arithmeticShader}`,
-  fs: null
+  vs: fp64arithmeticShader,
+  fs: null,
+  getUniforms,
+  fp64ify,
+  fp64LowPart
 };
 
-// Fragment shader fp64
-export const fp64fs = {
-  name: 'fp64-fs',
-  vs: null,
-  fs: fp64shader
+// Full fp64 shader
+export default {
+  name: 'fp64',
+  vs: fp64functionShader,
+  fs: null,
+  dependencies: [fp64arithmetic],
+  fp64ify,
+  fp64LowPart,
+  fp64ifyMatrix4
 };
