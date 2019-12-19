b591bbfd5a09362d993d09d1ab38db4171cfeb6e
Tarek Sherif <tsherif@uber.com>
2019-12-19 12:11:10 -0500

    Update website links to 8.0 (#1327)

--- scripts/update-release-branch.sh
+++ scripts/update-release-branch.sh
@@ -6,8 +6,7 @@ set -e
 
 BRANCH=`echo "$1-release"`
 VERSION=`echo "$1.0"`
-WEBSITE_PAGES=website/contents/pages.js
-WEBSITE_GATSBY_EXAMPLES=website-gatsby/src/components/animation-loop-example-page.jsx
+WEBSITE_EXAMPLES=website/src/components/animation-loop-example-page.jsx
 
 echo "Updating branch to ${BRANCH}..."
 
@@ -15,10 +14,9 @@ echo "Updating branch to ${BRANCH}..."
 find docs -iname "*.md" -type f -exec sed -i '' -E "s/luma.gl\/(tree|blob)\/master\/modules/luma.gl\/tree\/${BRANCH}\/modules/g" {} \;
 find docs -iname "*.md" -type f -exec sed -i '' -E "s/luma.gl\/(tree|blob)\/master\/examples/luma.gl\/tree\/${BRANCH}\/examples/g" {} \;
 
-# Replace source links in website
-sed -i '' -E "s/luma.gl\/(master|[1-9]*.[0-9]-release)/luma.gl\/${BRANCH}/g" "${WEBSITE_PAGES}"
-sed -i '' -E "s/luma.gl\/tree\/(master|[1-9]*.[0-9]-release)/luma.gl\/tree\/${BRANCH}/g" "${WEBSITE_PAGES}"
-sed -i '' -E "s/luma.gl\/tree\/(master|[1-9]*.[0-9]-release)/luma.gl\/tree\/${BRANCH}/g" "${WEBSITE_GATSBY_EXAMPLES}"
+# Replace source links in website (second one is for raw.githubusercontent)
+sed -i '' -E "s/luma.gl\/tree\/(master|[1-9]*.[0-9]-release)/luma.gl\/tree\/${BRANCH}/g" "${WEBSITE_EXAMPLES}"
+sed -i '' -E "s/luma.gl\/(master|[1-9]*.[0-9]-release)/luma.gl\/${BRANCH}/g" "${WEBSITE_EXAMPLES}"
 
 # Bump dependencies in examples
 update_dep() {
