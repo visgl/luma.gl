53c40d8967618bb8615cebc988b2dbc2304515b9
Tarek Sherif <tsherif@uber.com>
2020-01-24 18:10:45 -0500

    Docs

--- modules/shadertools/src/lib/transpile-shader.js
+++ modules/shadertools/src/lib/transpile-shader.js
@@ -17,12 +17,12 @@ const FS_OUTPUT_REGEX = /^[ \t]*out\s+vec4\s+(\w+)\s*;/m;
 function convertVertexShaderTo300(source) {
   return source
     .replace(/^(#version\s+(100|300\s+es))?[ \t]*\n/, '#version 300 es\n')
-    .replace(/attribute\s+/g, 'in ')
-    .replace(/varying\s+/g, 'out ')
-    .replace(/texture2D\(/g, 'texture(')
-    .replace(/textureCube\(+/g, 'texture(')
-    .replace(/texture2DLodEXT\(/g, 'textureLod(')
-    .replace(/textureCubeLodEXT\(/g, 'textureLod(');
+    .replace(/\battribute\s+/g, 'in ')
+    .replace(/\bvarying\s+/g, 'out ')
+    .replace(/\btexture2D\(/g, 'texture(')
+    .replace(/\btextureCube\(+/g, 'texture(')
+    .replace(/\btexture2DLodEXT\(/g, 'textureLod(')
+    .replace(/\btextureCubeLodEXT\(/g, 'textureLod(');
 }
 
 function convertFragmentShaderTo300(source) {
@@ -33,9 +33,6 @@ function convertFragmentShaderTo300(source) {
     .replace(/\btextureCube\(/g, 'texture(')
     .replace(/\btexture2DLodEXT\(/g, 'textureLod(')
     .replace(/\btextureCubeLodEXT\(/g, 'textureLod(');
-
-  // Deal with fragColor
-  // .replace(/gl_fragColor/g, 'fragColor ');
 }
 
 function convertVertexShaderTo100(source) {
@@ -44,7 +41,8 @@ function convertVertexShaderTo100(source) {
     .replace(/^#version\s+300\s+es/, '#version 100')
     .replace(/^[ \t]*in[ \t]+/gm, 'attribute ')
     .replace(/^[ \t]*out[ \t]+/gm, 'varying ')
-    .replace(/\btexture\(/g, 'texture2D(');
+    .replace(/\btexture\(/g, 'texture2D(')
+    .replace(/\btextureLod\(/g, 'texture2DLodEXT(');
 }
 /* eslint-disable */
 function convertFragmentShaderTo100(source) {
@@ -52,7 +50,8 @@ function convertFragmentShaderTo100(source) {
   source = source
     .replace(/^#version\s+300\s+es/, '#version 100')
     .replace(/^[ \t]*in[ \t]+/gm, 'varying ')
-    .replace(/\btexture\(/g, 'texture2D(');
+    .replace(/\btexture\(/g, 'texture2D(')
+    .replace(/\btextureLod\(/g, 'texture2DLodEXT(');
 
   const outputMatch = source.match(FS_OUTPUT_REGEX);
   if (outputMatch) {
