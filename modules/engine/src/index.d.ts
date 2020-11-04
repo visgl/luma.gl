4f303222b7c81c55598e68d873c994aa6e71dc93
Chris Gervang <chris@gervang.com>
2020-11-04 12:37:19 -0800

    Animation typings

--- modules/engine/src/index.d.ts
+++ modules/engine/src/index.d.ts
@@ -1,3 +1,4 @@
+/**
 //typings for @luma.gl/engine v8.1.2
 declare module "@luma.gl/engine/lib/animation-loop" {
 	export default class AnimationLoop {
@@ -386,3 +387,26 @@ declare module "geometries" {
 	export { default as SphereGeometry } from "@luma.gl/engine/geometries/sphere-geometry";
 	export { default as TruncatedConeGeometry } from "@luma.gl/engine/geometries/truncated-cone-geometry";
 }
+*/
+// CORE
+export {default as AnimationLoop} from './lib/animation-loop';
+export {default as Model} from './lib/model';
+export {default as ProgramManager} from './lib/program-manager';
+export {default as Transform} from './transform/transform';
+
+// Geometries
+export {default as Geometry} from './geometry/geometry';
+export {default as ConeGeometry} from './geometries/cone-geometry';
+export {default as CubeGeometry} from './geometries/cube-geometry';
+export {default as CylinderGeometry} from './geometries/cylinder-geometry';
+export {default as IcoSphereGeometry} from './geometries/ico-sphere-geometry';
+export {default as PlaneGeometry} from './geometries/plane-geometry';
+export {default as SphereGeometry} from './geometries/sphere-geometry';
+export {default as TruncatedConeGeometry} from './geometries/truncated-cone-geometry';
+
+// Animation
+export {Timeline} from './animation/timeline';
+export {KeyFrames} from './animation/key-frames';
+
+// Utils
+export {default as ClipSpace} from './utils/clip-space';
