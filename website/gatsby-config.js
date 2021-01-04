23c2beca4309ae24af6400c30b154706510caf71
Ib Green <ib@unfolded.ai>
2021-01-04 07:13:14 -0800

    website: gatsby setup update (#1422)

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,8 +1,251 @@
-const ocularConfig = require('./ocular-config');
+const {resolve} = require('path');
+const DOC_TABLE_OF_CONTENTS = require('../docs/table-of-contents.json');
 
-const GATSBY_CONFIG = {
+const ROOT_DIR = resolve('..');
+
+module.exports = {
   plugins: [
-    {resolve: `gatsby-theme-ocular`, options: ocularConfig},
+    {
+      resolve: `gatsby-theme-ocular`,
+      options: {
+        logLevel: 1, // Adjusts amount of debug information from ocular-gatsby
+
+        // Folders
+        DIR_NAME: __dirname,
+        ROOT_FOLDER: ROOT_DIR,
+        
+        DOCS: DOC_TABLE_OF_CONTENTS,
+        DOC_FOLDERS: [
+          resolve(ROOT_DIR, 'docs'),
+          resolve(ROOT_DIR, 'modules')
+        ],
+        SOURCE: [
+          resolve('./static'),
+          resolve('./src')
+        ],
+            
+
+        PROJECT_TYPE: 'github',
+      
+        PROJECT_NAME: 'luma.gl',
+        PROJECT_ORG: 'visgl',
+        PROJECT_ORG_LOGO: 'images/visgl-logo.png',
+        PROJECT_URL: `https://luma.gl`,
+        PROJECT_DESC: 'High-performance GPU Rendering and Compute Toolkit',
+        PROJECT_IMAGE: 'images/hero.png',
+      
+        PATH_PREFIX: '/luma.gl',
+      
+        LINK_TO_GET_STARTED: '/docs/getting-started',
+      
+        GA_TRACKING_ID: 'UA-74374017-2',
+      
+        // For showing star counts and contributors.
+        // Should be like btoa('YourUsername:YourKey') and should be readonly.
+        GITHUB_KEY: null,
+      
+        HOME_PATH: '',
+      
+        PROJECTS: [
+          {
+            name: 'vis.gl',
+            url: 'https://vis.gl'
+          },
+          {
+            name: 'deck.gl',
+            url: 'https://deck.gl'
+          },
+          {
+            name: 'math.gl', 
+            url: 'https://math.gl'
+          },
+          {
+            name: 'loaders.gl',
+            url: 'https://loaders.gl'
+          }
+        ],
+      
+        LINK_TO_GET_STARTED: '/docs/developer-guide/get-started',
+
+        ADDITIONAL_LINKS: [{
+          name: 'Blog',
+          href: 'http://medium.com/vis-gl',
+          index: 4
+        }],
+      
+        THEME_OVERRIDES: require('./templates/theme.json'),
+      
+        STYLESHEETS: ['/style.css'],
+      
+        INDEX_PAGE_URL: resolve(__dirname, './templates/index.jsx'),
+        PAGES: [
+          {
+            path: '/',
+            componentUrl: resolve(__dirname, './templates/index.jsx'),
+            content: ''
+          }
+        ],
+
+        EXAMPLES: [
+          {
+            title: 'Instancing',
+            componentUrl: resolve(__dirname, './templates/showcase/example-instancing.jsx'),
+            path: 'examples/showcase/instancing/',
+            image: 'images/example-instancing.jpg'
+          },
+          {
+            title: 'Geospatial',
+            componentUrl: resolve(__dirname, './templates/showcase/example-geospatial.jsx'),
+            path: 'examples/showcase/geospatial/',
+            image: 'images/example-geospatial.jpg'
+          },
+          {
+            title: 'Persistence',
+            componentUrl: resolve(__dirname, './templates/showcase/example-persistence.jsx'),
+            path: 'examples/showcase/persistence/',
+            image: 'images/example-persistence.jpg'
+          },
+          {
+            title: 'Wandering',
+            componentUrl: resolve(__dirname, './templates/showcase/example-wandering.jsx'),
+            path: 'examples/showcase/wandering/',
+            image: 'images/example-wandering.png'
+          },
+          {
+            title: 'DOF',
+            componentUrl: resolve(__dirname, './templates/showcase/example-dof.jsx'),
+            path: 'examples/showcase/dof/',
+            image: 'images/example-dof.jpg'
+          },
+          {
+            title: 'GLTF',
+            componentUrl: resolve(__dirname, './templates/showcase/example-gltf.jsx'),
+            path: 'examples/showcase/gltf/',
+            image: 'images/example-gltf.jpg'
+          },
+          {
+            title: 'Hello Triangle',
+            componentUrl: resolve(__dirname, './templates/getting-started/example-hello-triangle.jsx'),
+            path: 'examples/getting-started/hello-triangle',
+            image: 'images/example-hello-triangle.png'
+          },
+          {
+            title: 'Hello Instancing - High-level',
+            componentUrl: resolve(
+              __dirname,
+              './templates/getting-started/example-hello-instancing-high.jsx'
+            ),
+            path: 'examples/getting-started/hello-instancing-high',
+            image: 'images/example-hello-instancing.png'
+          },
+          {
+            title: 'Hello Instancing - Mid-level',
+            componentUrl: resolve(
+              __dirname,
+              './templates/getting-started/example-hello-instancing-mid.jsx'
+            ),
+            path: 'examples/getting-started/hello-instancing-mid',
+            image: 'images/example-hello-instancing.png'
+          },
+          {
+            title: 'Hello Instancing - Low-level',
+            componentUrl: resolve(
+              __dirname,
+              './templates/getting-started/example-hello-instancing-low.jsx'
+            ),
+            path: 'examples/getting-started/hello-instancing-low',
+            image: 'images/example-hello-instancing.png'
+          },
+          {
+            title: 'Shader Modules',
+            componentUrl: resolve(__dirname, './templates/getting-started/example-shader-modules.jsx'),
+            path: 'examples/getting-started/shader-modules',
+            image: 'images/example-shader-modules.png'
+          },
+          {
+            title: 'Shader Hooks',
+            componentUrl: resolve(__dirname, './templates/getting-started/example-shader-hooks.jsx'),
+            path: 'examples/getting-started/shader-hooks',
+            image: 'images/example-shader-hooks.jpg'
+          },
+          {
+            title: 'Shader Modules - Low-level',
+            componentUrl: resolve(
+              __dirname,
+              './templates/getting-started/example-shader-modules-low.jsx'
+            ),
+            path: 'examples/getting-started/shader-modules-low',
+            image: 'images/example-shader-hooks.jpg'
+          },
+          {
+            title: 'Transform Feedback',
+            componentUrl: resolve(
+              __dirname,
+              './templates/getting-started/example-transform-feedback.jsx'
+            ),
+            path: 'examples/getting-started/transform-feedback',
+            image: 'images/example-transform-feedback.jpg'
+          },
+          {
+            title: 'External Context',
+            componentUrl: resolve(__dirname, './templates/getting-started/example-external-context.jsx'),
+            path: 'examples/getting-started/external-context',
+            image: 'images/example-hello-triangle.png'
+          },
+          {
+            title: 'Hello Cube',
+            componentUrl: resolve(__dirname, './templates/getting-started/example-hello-cube.jsx'),
+            path: 'examples/getting-started/hello-cube/',
+            image: 'images/example-hello-cube.jpg'
+          },
+          {
+            title: 'Lighting',
+            componentUrl: resolve(__dirname, './templates/getting-started/example-lighting.jsx'),
+            path: 'examples/getting-started/lighting/',
+            image: 'images/example-lighting.jpg'
+          },
+          {
+            title: 'Instanced Transform',
+            componentUrl: resolve(
+              __dirname,
+              './templates/getting-started/example-instanced-transform.jsx'
+            ),
+            path: 'examples/getting-started/instanced-transform/',
+            image: 'images/example-instanced-transform.jpg'
+          },
+          {
+            title: 'Animation',
+            componentUrl: resolve(__dirname, './templates/api/example-animation.jsx'),
+            path: 'examples/api/animation/',
+            image: 'images/example-animation.png'
+          },
+          {
+            title: 'Program Management',
+            componentUrl: resolve(__dirname, './templates/api/example-program-management.jsx'),
+            path: 'examples/api/program-management/',
+            image: 'images/example-program-management.png'
+          },
+          {
+            title: 'Cubemap',
+            componentUrl: resolve(__dirname, './templates/api/example-cubemap.jsx'),
+            path: 'examples/api/cubemap/',
+            image: 'images/example-cubemap.jpg'
+          },
+          {
+            title: 'Texture3D',
+            componentUrl: resolve(__dirname, './templates/api/example-texture-3d.jsx'),
+            path: 'examples/api/texture3d/',
+            image: 'images/example-texture3d.png'
+          },
+          {
+            title: 'Stress Test',
+            componentUrl: resolve(__dirname, './templates/performance/example-stress-test.jsx'),
+            path: 'examples/performance/stress-test/',
+            image: 'images/example-stress-test.png'
+          }
+        ]      
+      }
+    },
     {
       resolve: 'gatsby-plugin-env-variables',
       options: {
@@ -12,8 +255,3 @@ const GATSBY_CONFIG = {
     `gatsby-plugin-no-sourcemaps`
   ]
 };
-
-// NOTE: uncomment to debug config
-// console.log(JSON.stringify(GATSBY_CONFIG, null, 2));
-
-module.exports = GATSBY_CONFIG;
--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -1,68 +1,42 @@
-const {setOcularConfig} = require('gatsby-theme-ocular');
-const {onCreateWebpackConfig} = require('gatsby-theme-ocular/gatsby-node');
-
-const ocularConfig = require('./ocular-config');
-setOcularConfig(ocularConfig);
+const resolve = require('path').resolve;
+const getOcularConfig = require('ocular-dev-tools/config/ocular.config');
+const DEPENDENCIES = require('./package.json').dependencies;
 
 module.exports.onCreateWebpackConfig = function onCreateWebpackConfigOverride(opts) {
-  onCreateWebpackConfig(opts);
-
   const {
     stage, // build stage: ‘develop’, ‘develop-html’, ‘build-javascript’, or ‘build-html’
     // rules, // Object (map): set of preconfigured webpack config rules
     // plugins, // Object (map): A set of preconfigured webpack config plugins
-    // getConfig, // Function that returns the current webpack config
-    loaders, // Object (map): set of preconfigured webpack config loaders
+    getConfig, // Function that returns the current webpack config
+    // loaders, // Object (map): set of preconfigured webpack config loaders
     actions
   } = opts;
 
-  console.log(`App rewriting gatsby webpack config`); // eslint-disable-line
+  console.log(`App rewriting gatsby webpack config ${stage}`); // eslint-disable-line
 
-  // Recreate it with custom exclude filter
-  // Called without any arguments, `loaders.js` will return an
-  // object like:
-  // {
-  //   options: undefined,
-  //   loader: '/path/to/node_modules/gatsby/dist/utils/babel-loader.js',
-  // }
-  // Unless you're replacing Babel with a different transpiler, you probably
-  // want this so that Gatsby will apply its required Babel
-  // presets/plugins.  This will also merge in your configuration from
-  // `babel.config.js`.
-  const newJSRule = loaders.js();
+  const config = getConfig();
+  config.resolve = config.resolve || {};
+  config.resolve.alias = config.resolve.alias || {};
 
-  Object.assign(newJSRule, {
-    // JS and JSX
-    test: /\.jsx?$/,
+  const ALIASES = getOcularConfig({root: resolve(__dirname, '..')}).aliases;
 
-    // Exclude all node_modules from transpilation, except for ocular
-    exclude: modulePath =>
-      /node_modules/.test(modulePath) &&
-      !/node_modules\/(ocular|ocular-gatsby|gatsby-theme-ocular)/.test(modulePath)
-  });
-
-  // Omit the default rule where test === '\.jsx?$'
-  const rules = [newJSRule];
-
-  if (stage === 'build-html') {
-    rules.push({
-      test: /mapbox-gl/,
-      use: loaders.null()
-    });
+  // When duplicating example dependencies in website, autogenerate
+  // aliases to ensure the website version is picked up
+  // NOTE: module dependencies are automatically injected
+  // TODO - should this be automatically done by ocular-gatsby?
+  const dependencyAliases = {};
+  for (const dependency in DEPENDENCIES) {
+    dependencyAliases[dependency] = `${__dirname}/node_modules/${dependency}`;
   }
 
-  const newConfig = {
-    module: {
-      rules
-    },
-    node: {
-      fs: 'empty'
-    }
-  };
+  // This looks strange but order matters...
+  Object.assign(config.resolve.alias, ALIASES, dependencyAliases, ALIASES);
+
+  // console.error(JSON.stringify(config.resolve.alias, null, 2));
 
   // Completely replace the webpack config for the current stage.
   // This can be dangerous and break Gatsby if certain configuration options are changed.
   // Generally only useful for cases where you need to handle config merging logic yourself,
   // in which case consider using webpack-merge.
-  actions.setWebpackConfig(newConfig);
+  actions.replaceWebpackConfig(config);
 };
