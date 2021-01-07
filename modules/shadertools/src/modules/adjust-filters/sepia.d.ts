1204cb114a99cb33bdb79860e143f7498ff908ab
Ib Green <ib@unfolded.ai>
2021-01-07 15:43:52 -0800

    wip

--- modules/shadertools/src/modules/adjust-filters/sepia.d.ts
+++
@@ -1,8 +0,0 @@
-import {ShaderPass} from '../../types';
-
-/**
- * Sepia -
- * Gives the image a reddish-brown monochrome tint that imitates an old photograph.
- * @param amount   0 to 1 (0 for no effect, 1 for full sepia coloring)
- */
-export const brightnessContrast: ShaderPass;
--- modules/shadertools/src/modules/fun-filters/edgework.d.ts
+++
@@ -1,9 +0,0 @@
-import {ShaderPass} from '../../types';
-
-/**
- * Edge Work -
- * Picks out different frequencies in the image by subtracting two
- * copies of the image blurred with different radii.
- * @param radius The radius of the effect in pixels.
- */
-export const edgeWork: ShaderPass;
