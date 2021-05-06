ec334ca61481da149ac8f6e623a57cb963d50ad5
Xintong Xia <xxia@aurora.tech>
2021-05-05 21:12:30 -0700

    rename and fix

--- modules/shadertools/src/modules/image-fun-filters/magnify.d.ts
+++ modules/shadertools/src/modules/image-fun-filters/magnify.d.ts
@@ -2,7 +2,7 @@ import {ShaderPass} from '../../types';
 
 /**
  * Magnify - display a circle with magnify effect applied to surrounding the pixels given position
- * @param position: position on screen coords, both x and y is normalized and in range [0, 1] 
+ * @param screenXY: position on screen coords, both x and y is normalized and in range [0, 1] 
  * @param radiusPixels: effect radius in pixels 
  * @param zoom: magnify level 
  * @param borderWidthPixels: border width of the circle, donnot show border when <= 0.0
--- modules/shadertools/src/modules/image-fun-filters/magnify.js
+++ modules/shadertools/src/modules/image-fun-filters/magnify.js
@@ -1,12 +1,12 @@
 const fs = `\
-uniform vec2 position;
+uniform vec2 screenXY;
 uniform float radiusPixels;
 uniform float zoom;
 uniform float borderWidthPixels;
 uniform vec4 borderColor;
 
 vec4 magnify_sampleColor(sampler2D texture, vec2 texSize, vec2 texCoord) {
-  vec2 pos = vec2(position.x, 1.0 - position.y);
+  vec2 pos = vec2(screenXY.x, 1.0 - screenXY.y);
   float dist = distance(texCoord * texSize, pos * texSize);
   if (dist < radiusPixels) {
     return texture2D(texture, (texCoord - pos) / zoom + pos);
@@ -21,16 +21,16 @@ vec4 magnify_sampleColor(sampler2D texture, vec2 texSize, vec2 texCoord) {
 
 const uniforms = {
   // range 0 to 1
-  position: [0, 0],
+  screenXY: [0, 0],
   radiusPixels: 200,
   zoom: 2.0,
   borderWidthPixels: 0.0,
-  borderColor: [255, 255, 255, 255],
+  borderColor: [255, 255, 255, 255]
 };
 
-export const MagnifyEffect = {
+export const magnify = {
   name: 'magnify',
   uniforms,
   fs,
-  passes: [{sampler: true}],
+  passes: [{sampler: true}]
 };
