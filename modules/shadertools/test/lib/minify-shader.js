80c0f680c6219bbe338fa5e221b27655ebfc1618
Jan Žák <zj@zakjan.cz>
2021-07-24 19:50:54 +0200

    Fix transpiling minified shader (#1482)

---
+++ modules/shadertools/test/lib/minify-shader.js
@@ -0,0 +1,23 @@
+// copied from rollup-plugin-glslify
+export function minifyShader(code) {
+  let needNewline = false;
+  return code
+    .replace(/\\(?:\r\n|\n\r|\n|\r)|\/\*.*?\*\/|\/\/(?:\\(?:\r\n|\n\r|\n|\r)|[^\n\r])*/g, '')
+    .split(/\n+/)
+    .reduce((result, line) => {
+      line = line.trim().replace(/\s{2,}|\t/, ' ');
+      if (line.charAt(0) === '#') {
+        if (needNewline) {
+          result.push('\n');
+        }
+        result.push(line, '\n');
+        needNewline = false;
+      } else {
+        result.push(line.replace(/\s*({|}|=|\*|,|\+|\/|>|<|&|\||\[|\]|\(|\)|-|!|;)\s*/g, '$1'));
+        needNewline = true;
+      }
+      return result;
+    }, [])
+    .join('')
+    .replace(/\n+/g, '\n');
+}
