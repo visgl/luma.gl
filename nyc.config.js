7b8ba35b53a94ed44732c4aaa94a3762f418a335
Ib Green <ib@unfolded.ai>
2021-12-23 07:58:28 -0800

    chore: exclude modules/webgpu from coverage (#1564)

--- nyc.config.js
+++ nyc.config.js
@@ -4,14 +4,20 @@ module.exports = {
   all: 'true',
   sourceMap: false,
   instrument: true,
-  extensions: ['.ts'],
+  extensions: ['.js', '.ts'],
   include: ['dev-modules', 'modules/**/src'],
   exclude: [
     '**/deprecated',
     '**/bundle.ts',
+    // no need to test coverage of constants
     'modules/constants',
+    // core is just re-exporting things at the moment
     'modules/core',
+    // debug is an optional dependency
     'modules/debug',
-    'modules/test-utils/'
+    // we don't have a test setup for WebGPU yet
+    'modules/webgpu',
+    // these are test utilities
+    'modules/test-utils'
   ]
 };
