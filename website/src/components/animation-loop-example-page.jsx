f12c076f24e1d6830f2a16e2832c8f939f123db5
Ib Green <ib@unfolded.ai>
2022-01-04 11:41:47 -0800

    feat(focs): Live examples in tutorials (#1582)

--- website/src/components/animation-loop-example-page.jsx
+++ website/src/components/animation-loop-example-page.jsx
@@ -191,7 +191,7 @@ export default class AnimationLoopExamplePage extends Component {
           style={{width: '100%', height: '100%'}}
         />
         {panel && (
-          <InfoPanel title={title} sourceLink={`${GITHUB_TREE}/${path}`}>
+          <InfoPanel title={title} sourceLink={`${GITHUB_TREE}/${path}`} style={{opacity: 0.6}}>
             <div dangerouslySetInnerHTML={{__html: controls}} />
           </InfoPanel>
         )}
