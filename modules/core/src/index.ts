01feb22a810164cbfce44d64d6987f451a4b554a
Ib Green <ib@unfolded.ai>
2022-12-22 15:19:23 -0500

    wip

--- modules/core/src/index.ts
+++ modules/core/src/index.ts
@@ -1,14 +1,21 @@
 // luma.gl, MIT license
-// core module exports
+
+// core module re-exports API and engine
+
+// @luma.gl/api exports
+// TODO - export full api in v10 (names conflict with removed WebGL exports)
+
+export type {DeviceProps} from '@luma.gl/api';
+export {Device, luma} from '@luma.gl/api';
 
 // UTILS
 export {log, assert, uid} from '@luma.gl/api';
 
-// TODO in v10 - export @luma.gl/api
-
 // @luma.gl/engine exports
+
 export {Timeline} from '@luma.gl/engine';
 
+// TODO - these still have breaking changes, will be upgraded later
 export {
   ClassicAnimationLoop as AnimationLoop,
   ClassicModel as Model,
@@ -16,5 +23,3 @@ export {
   ProgramManager,
   ClipSpace
 } from '@luma.gl/webgl-legacy';
-
-// TODO - the following exports will be removed in v9
