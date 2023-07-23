6619ba2111890265b0bf12f6d98f268cb9a266bc
Ib Green <ib@unfolded.ai>
2023-07-23 12:41:16 -0400

    wip

--- modules/shadertools/src/lib/generator/utils/capitalize.ts
+++
@@ -1,9 +0,0 @@
-
-/**
- * Capitalize first letter of a string
- * @param {string} str
- * @returns {string}
- */
-export function capitalize(str: string): string {
-  return typeof str === 'string' ? str.charAt(0).toUpperCase() + str.slice(1) : str;
-}
