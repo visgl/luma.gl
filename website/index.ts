119f32264df5e47c5a1380d033543fd0d81966de
Ib Green <ib@foursquare.com>
2023-11-11 07:34:52 -0500

    fix(website): improve example code (#1834)

--- website/index.ts
+++ website/index.ts
@@ -1,25 +1,26 @@
 // Tutorials
-export {HelloTriangleExample} from './src/examples/templates/tutorials/example-hello-triangle';
-export {HelloCubeExample} from './src/examples/templates/tutorials/example-hello-cube';
-export {LightingExample} from './src/examples/templates/tutorials/example-lighting';
-export {HelloInstancingExample} from './src/examples/templates/tutorials/example-hello-instancing';
-export {InstancedTransformExample} from './src/examples/templates/tutorials/example-instanced-transform';
-export {ShaderModulesExample} from './src/examples/templates/tutorials/example-shader-modules';
-export {ShaderHooksExample} from './src/examples/templates/tutorials/example-shader-hooks';
-export {TransformExample} from './src/examples/templates/tutorials/example-transform';
-export {TransformFeedbackExample} from './src/examples/templates/tutorials/example-transform-feedback';
+export {
+  HelloTriangleExample,
+  HelloCubeExample,
+  LightingExample,
+  HelloInstancingExample,
+  InstancedTransformExample,
+  ShaderModulesExample,
+  ShaderHooksExample,
+  TransformExample,
+  TransformFeedbackExample,
 
-export {InstancingExample} from './src/examples/templates/showcase/example-instancing';
-export {PersistenceExample} from './src/examples/templates/showcase/example-persistence';
+  // GeospatialExample
+  InstancingExample,
+  PersistenceExample,
 
-export {AnimationExample} from './src/examples/templates/api/example-animation';
-export {CubemapExample} from './src/examples/templates/api/example-cubemap';
-export {Texture3DExample} from './src/examples/templates/api/example-texture-3d';
+  AnimationExample,
+  CubemapExample,
+  Texture3DExample,
 
-export {HelloTriangleWebGPUExample} from './src/examples/templates/webgpu/example-hello-triangle-webgpu';
-export {InstancedCubesWebGPUExample} from './src/examples/templates/webgpu/example-instanced-cubes-webgpu';
-export {RotatingCubeWebGPUExample} from './src/examples/templates/webgpu/example-rotating-cube-webgpu';
-export {TexturedCubeWebGPUExample} from './src/examples/templates/webgpu/example-textured-cube-webgpu';
-export {TwoCubesWebGPUExample} from './src/examples/templates/webgpu/example-two-cubes-webgpu';
-
-// export {GeospatialExample} from './templates/showcase/example-geospatial';
+  HelloTriangleWebGPUExample,
+  InstancedCubesWebGPUExample,
+  RotatingCubeWebGPUExample,
+  TexturedCubeWebGPUExample,
+  TwoCubesWebGPUExample
+} from './src/examples/templates';
