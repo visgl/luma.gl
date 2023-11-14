8f1b5429cf91ac17e212855915436c7baeec3c85
Ib Green <ib@foursquare.com>
2023-11-13 20:33:08 -0500

    fix

--- modules/shadertools/src/lib/shader-module/normalize-shader-module.ts
+++ modules/shadertools/src/lib/shader-module/normalize-shader-module.ts
@@ -3,7 +3,7 @@ import { ShaderModuleInstance } from './shader-module-instance';
 export function normalizeShaderModule(module: any): any {
   if (!module.normalized) {
     module.normalized = true;
-    if (module.uniforms && !module.getUniforms) {
+    if (module.uniformPropTypes && !module.getUniforms) {
       const shaderModule = new ShaderModuleInstance(module);
       module.getUniforms = shaderModule.getUniforms.bind(shaderModule);
     }
