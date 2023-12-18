58e31d5c572ef41e271f072725b3dd62ba9b7a3c
Ib Green <ib@foursquare.com>
2023-12-18 09:03:19 -0500

    chore(shadertools): Improve generated shaders and ShaderModule typing

--- modules/shadertools/src/index.ts
+++ modules/shadertools/src/index.ts
@@ -128,39 +128,6 @@ export {fxaa} from './modules/postprocessing/fxaa/fxaa';
 export type {WarpProps} from './modules/postprocessing/image-warp-filters/warp';
 export {warp as _warp} from './modules/postprocessing/image-warp-filters/warp';
 
-// // glfx BLUR shader modules
-// export {tiltShift} from './modules/postprocessing/image-blur-filters/tiltshift';
-// export {triangleBlur} from './modules/postprocessing/image-blur-filters/triangleblur';
-// export {zoomBlur} from './modules/postprocessing/image-blur-filters/zoomblur';
-
-// // glfx image adjustment shader modules
-// export type {BrightnessContrastProps} from './modules/postprocessing/image-adjust-filters/brightnesscontrast';
-// export {brightnessContrast} from './modules/postprocessing/image-adjust-filters/brightnesscontrast';
-// export {denoise} from './modules/postprocessing/image-adjust-filters/denoise';
-// export {hueSaturation} from './modules/postprocessing/image-adjust-filters/huesaturation';
-// export {noise} from './modules/postprocessing/image-adjust-filters/noise';
-// export {sepia} from './modules/postprocessing/image-adjust-filters/sepia';
-// export {vibrance} from './modules/postprocessing/image-adjust-filters/vibrance';
-// export {vignette} from './modules/postprocessing/image-adjust-filters/vignette';
-
-// // glfx FUN shader modules
-// export {colorHalftone} from './modules/postprocessing/image-fun-filters/colorhalftone';
-// export {dotScreen} from './modules/postprocessing/image-fun-filters/dotscreen';
-// export {edgeWork} from './modules/postprocessing/image-fun-filters/edgework';
-// export {hexagonalPixelate} from './modules/postprocessing/image-fun-filters/hexagonalpixelate';
-// export {ink} from './modules/postprocessing/image-fun-filters/ink';
-// export {magnify} from './modules/postprocessing/image-fun-filters/magnify';
-
-// // glfx WARP shader modules
-// export {bulgePinch} from './modules/postprocessing/image-warp-filters/bulgepinch';
-// export {swirl} from './modules/postprocessing/image-warp-filters/swirl';
-
-// // Postprocessing
-// export {fxaa} from './modules/postprocessing/fxaa/fxaa';
-
-// // experimental
-// export {warp as _warp} from './modules/postprocessing/image-warp-filters/warp';
-// // export {transform as _transform} from './modules/postprocessing/transform/transform';
 
 // DEPRECATED - v8 legacy shader modules (non-uniform buffer)
 
