4ffdcce86df845596eee4db32e7517518d42b595
郭斌勇 <guobinyong@qq.com>
2023-12-21 09:46:42 +0800

    refactor(engine): AnimationLoop start

--- modules/engine/src/animation-loop/animation-loop.ts
+++ modules/engine/src/animation-loop/animation-loop.ts
@@ -148,10 +148,6 @@ export class AnimationLoop {
     this._running = true;
 
     try {
-      // check that we haven't been stopped
-      if (!this._running) {
-        return null;
-      }
 
       let appContext;
       if (!this._initialized) {
