fbf29a2c344ce04cc5574963aeecf7c8db7de0f7
Felix Palmer <felixpalmer@gmail.com>
2024-01-15 15:46:04 +0100

    Tidy after merge

--- examples/showcase/instancing/app.ts
+++ examples/showcase/instancing/app.ts
@@ -293,7 +293,7 @@ export default class AppAnimationLoopTemplate extends AnimationLoopTemplate {
     }
 
     this.shaderInputs.setProps({
-      picking: {isActive: false, isHighlightActive, highlightedObjectColor}
+      picking: {isActive: false, highlightedObjectColor}
     });
   }
 }
