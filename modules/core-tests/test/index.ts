d9311c2eecd67feb042bfde0e0f259421ff99be0
Ib Green <ib@foursquare.com>
2024-02-28 07:47:43 -0500

    wip

--- modules/core-tests/test/index.ts
+++ modules/core-tests/test/index.ts
@@ -2,12 +2,6 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-// Note that we do two test runs on luma.gl, with and without headless-gl
-// This file imports tests that should run *with* headless-gl included
-
-// import './adapter/helpers/parse-shader-compiler-log.spec';
-// import './adapter/helpers/get-shader-layout.spec';
-
 // ADAPTER
 
 // WebGLDevice, features & limits
@@ -16,8 +10,11 @@ import './adapter/device-helpers/device-features.spec';
 import './adapter/device-helpers/device-limits.spec';
 import './adapter/device-helpers/set-device-parameters.spec';
 
-// import './adapter/device.spec';
-// import './adapter/canvas-context.spec';
+import './adapter/helpers/parse-shader-compiler-log.spec';
+// import './adapter/helpers/get-shader-layout.spec';
+
+import './adapter/device.spec';
+import './adapter/canvas-context.spec';
 
 // Resources
 import './adapter/texture-formats.spec';
@@ -31,4 +28,4 @@ import './adapter/resources/shader.spec';
 import './adapter/resources/sampler.spec';
 import './adapter/resources/texture.spec';
 import './adapter/resources/vertex-array.spec';
-import './adapter/resources/query.spec';
+import './adapter/resources/query-set.spec';
