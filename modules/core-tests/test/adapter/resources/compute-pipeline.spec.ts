757ec66527aa794dd53090bcd3edc1f2f61d5466
Ib Green <ib@foursquare.com>
2024-03-01 09:32:14 -0500

    wip

--- modules/core-tests/test/adapter/resources/compute-pipeline.spec.ts
+++ modules/core-tests/test/adapter/resources/compute-pipeline.spec.ts
@@ -19,47 +19,50 @@ const source = /* WGSL*/ `\
 
 test('ComputePipeline construct/delete', async t => {
   await getTestDevices();
-  const shader = webgpuDevice.createShader({source});
-  const computePipeline = webgpuDevice.createComputePipeline({shader});
-  t.ok(computePipeline instanceof ComputePipeline, 'ComputePipeline construction successful');
-  computePipeline.destroy();
-  t.ok(computePipeline instanceof ComputePipeline, 'ComputePipeline delete successful');
-  computePipeline.destroy();
-  t.ok(computePipeline instanceof ComputePipeline, 'ComputePipeline repeated delete successful');
+  if (webgpuDevice) {
+    const shader = webgpuDevice.createShader({source});
+    const computePipeline = webgpuDevice.createComputePipeline({shader});
+    t.ok(computePipeline instanceof ComputePipeline, 'ComputePipeline construction successful');
+    computePipeline.destroy();
+    t.ok(computePipeline instanceof ComputePipeline, 'ComputePipeline delete successful');
+    computePipeline.destroy();
+    t.ok(computePipeline instanceof ComputePipeline, 'ComputePipeline repeated delete successful');
+  }
   t.end();
 });
 
 test('ComputePipeline compute', async t => {
   await getTestDevices();
-  const shader = webgpuDevice.createShader({source});
-  const computePipeline = webgpuDevice.createComputePipeline({
-    shader,
-    shaderLayout: {
-      bindings: [{name: 'data', type: 'storage', location: 0}]
-    }
-  });
+  if (webgpuDevice) {
+    const shader = webgpuDevice.createShader({source});
+    const computePipeline = webgpuDevice.createComputePipeline({
+      shader,
+      shaderLayout: {
+        bindings: [{name: 'data', type: 'storage', location: 0}]
+      }
+    });
 
-  const workBuffer = webgpuDevice.createBuffer({
-    id: 'work buffer',
-    byteLength: 4,
-    usage: Buffer.STORAGE | Buffer.COPY_SRC | Buffer.COPY_DST
-  });
+    const workBuffer = webgpuDevice.createBuffer({
+      id: 'work buffer',
+      byteLength: 4,
+      usage: Buffer.STORAGE | Buffer.COPY_SRC | Buffer.COPY_DST
+    });
 
-  workBuffer.write(new Int32Array([2]));
-  const inputData = new Int32Array(await workBuffer.readAsync());
-  t.equal(inputData[0], 2, 'Input data is correct');
+    workBuffer.write(new Int32Array([2]));
+    const inputData = new Int32Array(await workBuffer.readAsync());
+    t.equal(inputData[0], 2, 'Input data is correct');
 
-  computePipeline.setBindings({data: workBuffer});
+    computePipeline.setBindings({data: workBuffer});
 
-  const computePass = webgpuDevice.beginComputePass({});
-  computePass.setPipeline(computePipeline);
-  computePass.dispatch(1);
-  computePass.end();
+    const computePass = webgpuDevice.beginComputePass({});
+    computePass.setPipeline(computePipeline);
+    computePass.dispatch(1);
+    computePass.end();
 
-  webgpuDevice.submit();
+    webgpuDevice.submit();
 
-  const computedData = new Int32Array(await workBuffer.readAsync());
-  t.equal(computedData[0], 4, 'Computed data is correct');
-
+    const computedData = new Int32Array(await workBuffer.readAsync());
+    t.equal(computedData[0], 4, 'Computed data is correct');
+  }
   t.end();
 });
