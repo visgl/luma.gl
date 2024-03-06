06de0cf8ff095f65fde7fca6e47c4d285684e06b
Xiaoji Chen <cxiaoji@gmail.com>
2024-03-06 13:39:15 -0800

    Fix pre-built bundle setup

--- modules/constants/src/bundle.ts
+++
@@ -1,7 +0,0 @@
-// luma.gl
-// SPDX-License-Identifier: MIT
-// Copyright (c) vis.gl contributors
-
-import {GL as GLConstants} from './index';
-
-export {GLConstants};
---
+++ modules/webgl/bundle.ts
@@ -0,0 +1,3 @@
+// Re-export core API so they don't get overwritten
+export * from '@luma.gl/core';
+export * from '@luma.gl/webgl';
