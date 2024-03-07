c801d597187fdf7d327fcbcddd0657c73a1105a5
felixpalmer <felixpalmer@gmail.com>
2024-03-07 22:09:33 +0100

    shadertools: Remove getVersionDefines (#2012)

--- modules/shadertools/src/modules/lighting/pbr-material/pbr-fragment-glsl.ts
+++ modules/shadertools/src/modules/lighting/pbr-material/pbr-fragment-glsl.ts
@@ -12,14 +12,6 @@
 import {glsl} from '../../../lib/glsl-utils/highlight';
 
 export const fs = glsl`\
-#if defined(USE_TEX_LOD) && !defined(FEATURE_GLSL_TEXTURE_LOD)
-# error PBR fragment shader: Texture LOD is not available
-#endif
-
-#if !defined(HAS_TANGENTS) && !defined(FEATURE_GLSL_DERIVATIVES)
-# error PBR fragment shader: Derivatives are not available
-#endif
-
 precision highp float;
 
 uniform Projection {
@@ -366,7 +358,7 @@ vec4 pbr_filterColor(vec4 colorUnused)
     color += calculateFinalColor(pbrInfo, lighting_uAmbientLight.color);
 
     // Apply directional light
-    for(int i = 0,  i < lighting_uDirectionalLightCount, i++) {
+    for(int i = 0; i < lighting_uDirectionalLightCount; i++) {
       if (i < lighting_uDirectionalLightCount) {
         PBRInfo_setDirectionalLight(pbrInfo, lighting_uDirectionalLight[i].direction);
         color += calculateFinalColor(pbrInfo, lighting_uDirectionalLight[i].color);
@@ -374,7 +366,7 @@ vec4 pbr_filterColor(vec4 colorUnused)
     }
 
     // Apply point light
-    for(int i = 0, i < lighting_uPointLightCount, i++) {
+    for(int i = 0; i < lighting_uPointLightCount; i++) {
       if (i < lighting_uPointLightCount) {
         PBRInfo_setPointLight(pbrInfo, lighting_uPointLight[i]);
         float attenuation = getPointLightAttenuation(lighting_uPointLight[i], distance(lighting_uPointLight[i].position, pbr_vPosition));
