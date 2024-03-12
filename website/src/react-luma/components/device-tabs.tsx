8a27179a13dec181ab7806af9b637f216ff8817a
Don McCurdy <donmccurdy@cartodb.com>
2024-03-12 15:30:40 -0400

    feat(website): Add 'devices' prop to DeviceTabs component (#2020)

--- website/src/react-luma/components/device-tabs.tsx
+++ website/src/react-luma/components/device-tabs.tsx
@@ -4,23 +4,39 @@ import BrowserOnly from '@docusaurus/BrowserOnly';
 import {Tabs, Tab} from './tabs';
 import {useStore} from '../store/device-store';
 
-export const DeviceTabsPriv = props => {
+interface DeviceTabsProps {
+  devices?: ('webgl2' | 'webgpu')[];
+}
+
+const DEFAULT_DEVICE_TABS_PROPS: Required<DeviceTabsProps> = {
+  devices: ['webgl2', 'webgpu']
+};
+
+export const DeviceTabsPriv = (props?: DeviceTabsProps) => {
+  props = {...DEFAULT_DEVICE_TABS_PROPS, ...props};
   const deviceType = useStore(state => state.deviceType);
   const deviceError = useStore(state => state.deviceError);
   const setDeviceType = useStore(state => state.setDeviceType);
 
   return (
     <Tabs selectedItem={deviceType} setSelectedItem={setDeviceType}>
-      <Tab key="WebGL2" title="WebGL2" tag="webgl">
-        {/* <img height="80" src="https://raw.github.com/visgl/deck.gl-data/master/images/whats-new/webgl2.jpg" />*/}
-        {deviceError}
-      </Tab>
-      <Tab key="WebGPU" title="WebGPU" tag="webgpu">
-        {/* <img height="80" src="https://raw.githubusercontent.com/gpuweb/gpuweb/3b3a1632ff1ad6a573330a58710e341bb9d65576/logo/webgpu-horizontal.svg" /> */}
-        {deviceError}
-      </Tab>
+      {props.devices.includes('webgl2') && (
+        <Tab key="WebGL2" title="WebGL2" tag="webgl">
+          {/* <img height="80" src="https://raw.github.com/visgl/deck.gl-data/master/images/whats-new/webgl2.jpg" />*/}
+          {deviceError}
+        </Tab>
+      )}
+
+      {props.devices.includes('webgpu') && (
+        <Tab key="WebGPU" title="WebGPU" tag="webgpu">
+          {/* <img height="80" src="https://raw.githubusercontent.com/gpuweb/gpuweb/3b3a1632ff1ad6a573330a58710e341bb9d65576/logo/webgpu-horizontal.svg" /> */}
+          {deviceError}
+        </Tab>
+      )}
     </Tabs>
   );
 };
 
-export const DeviceTabs = () => <BrowserOnly>{() => <DeviceTabsPriv />}</BrowserOnly>;
+export const DeviceTabs = (props?: DeviceTabsProps) => (
+  <BrowserOnly>{() => <DeviceTabsPriv {...props} />}</BrowserOnly>
+);
