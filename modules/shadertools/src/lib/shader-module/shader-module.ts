3557c19f87b2e15988b0078d4a0157dbc9ae5619
Felix Palmer <felixpalmer@gmail.com>
2024-07-25 09:52:51 +0200

    Remove NumberArray from UniformValue type

--- modules/shadertools/src/lib/shader-module/shader-module.ts
+++ modules/shadertools/src/lib/shader-module/shader-module.ts
@@ -2,26 +2,33 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-import {NumberArray} from '@math.gl/types';
 import {UniformFormat} from '../../types';
 import {PropType} from '../filters/prop-types';
 import {Buffer, Sampler, Texture} from '@luma.gl/core';
+import {UniformTypes} from '../utils/uniform-types';
 
-export type UniformValue = number | boolean | Readonly<NumberArray>; // Readonly<Float32Array> | Readonly<Int32Array> | Readonly<Uint32Array> | Readonly<number[]>;
+// Do not allow NumberArray as we cannot then typecheck on length
+export type UniformValue = number | boolean | number[];
 type BindingValue = Buffer | Texture | Sampler;
 
 export type UniformInfo = {
   format?: UniformFormat;
 } & PropType;
 
+// Helper types
+type FilterBindingKeys<T> = {[K in keyof T]: T[K] extends UniformValue ? never : K}[keyof T];
+type FilterUniformKeys<T> = {[K in keyof T]: T[K] extends UniformValue ? K : never}[keyof T];
+type BindingsOnly<T> = {[K in FilterBindingKeys<Required<T>>]: T[K]};
+type UniformsOnly<T> = {[K in FilterUniformKeys<Required<T>>]: T[K]};
+
 /**
  * A shader module definition object
  * @note Can be viewed as the ShaderModuleProps for a ShaderModuleInstance
  */
 export type ShaderModule<
-  PropsT extends Record<string, unknown> = Record<string, unknown>,
-  UniformsT extends Record<string, UniformValue> = Record<string, UniformValue>,
-  BindingsT extends Record<string, BindingValue> = Record<string, BindingValue>
+  PropsT extends Record<string, any> = Record<string, any>,
+  UniformsT extends Record<string, UniformValue> = UniformsOnly<PropsT>,
+  BindingsT extends Record<string, BindingValue> = BindingsOnly<PropsT>
 > = {
   /** Used for type inference not for values */
   props?: Required<PropsT>;
@@ -33,7 +40,7 @@ export type ShaderModule<
   vs?: string;
 
   /** Uniform shader types @note: Both order and types MUST match uniform block declarations in shader */
-  uniformTypes?: Record<keyof UniformsT, UniformFormat>;
+  uniformTypes?: Required<UniformTypes<UniformsT>>; // Record<keyof UniformsT, UniformFormat>;
   /** Uniform JS prop types  */
   uniformPropTypes?: Record<keyof UniformsT, UniformInfo>;
   /** Default uniform values */
