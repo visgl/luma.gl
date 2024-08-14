ade20bc2a6b964410208f58699f4e44ae6164d43
Ib Green <ib@foursquare.com>
2024-08-14 12:00:42 -0700

    fix

--- website/src/react-luma/components/luma-example.tsx
+++ website/src/react-luma/components/luma-example.tsx
@@ -88,6 +88,7 @@ export const LumaExample: FC<LumaExampleProps> = (props: LumaExampleProps) => {
       device = await luma.createDevice({
         adapters: [webgl2Adapter, webgpuAdapter],
         type: deviceType,
+        // @ts-expect-error
         createCanvasContext: {
           canvas,
           container: containerName
