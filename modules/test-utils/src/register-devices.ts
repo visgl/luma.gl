8e55d6069702a862793348bd13e1cf8d2a2e81da
Ib Green <ib@foursquare.com>
2024-09-23 13:36:10 -0400

    feat(test-utils): Avoid registering devices (#2275)

--- modules/test-utils/src/register-devices.ts
+++
@@ -1,9 +0,0 @@
-// luma.gl
-// SPDX-License-Identifier: MIT
-// Copyright (c) vis.gl contributors
-
-import {luma} from '@luma.gl/core';
-import {webgl2Adapter} from '@luma.gl/webgl';
-import {webgpuAdapter} from '@luma.gl/webgpu';
-
-luma.registerAdapters([webgl2Adapter, webgpuAdapter]);
