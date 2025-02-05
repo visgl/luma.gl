a7e2f7acdb181392e509e89a84b875697c6ab1f4
Ib Green <7025232+ibgreen@users.noreply.github.com>
2025-02-05 12:48:47 -0500

    fix(webgl): Add DeviceProps._reuseDevices to support deck.gl React StrictMode (#2320)
    Co-authored-by: Felix Palmer <felixpalmer@gmail.com>

--- modules/test-utils/src/null-device/null-device.ts
+++ modules/test-utils/src/null-device/null-device.ts
@@ -57,7 +57,7 @@ export class NullDevice extends Device {
   constructor(props: DeviceProps) {
     super({...props, id: props.id || 'null-device'});
 
-    const canvasContextProps = props.createCanvasContext === true ? {} : props.createCanvasContext;
+    const canvasContextProps = Device._getCanvasContextProps(props);
     this.canvasContext = new NullCanvasContext(this, canvasContextProps);
     this.lost = new Promise(resolve => {});
     this.canvasContext.resize();
