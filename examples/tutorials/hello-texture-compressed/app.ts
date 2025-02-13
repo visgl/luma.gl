50beac8b1af44b48f81daf61b5f82f02130f28ea
Don McCurdy <donmccurdy@cartodb.com>
2025-02-13 17:44:18 -0500

    fixup! include sRGB OETF

--- examples/tutorials/hello-texture-compressed/app.ts
+++ examples/tutorials/hello-texture-compressed/app.ts
@@ -100,9 +100,14 @@ in vec4 fragPosition;
 
 layout (location=0) out vec4 fragColor;
 
+vec4 sRGBTransferOETF( in vec4 value ) {
+	return vec4( mix( pow( value.rgb, vec3( 0.41666 ) ) * 1.055 - vec3( 0.055 ), value.rgb * 12.92, vec3( lessThanEqual( value.rgb, vec3( 0.0031308 ) ) ) ), value.a );
+}
+
 void main() {
   fragColor = fragPosition;
   fragColor = texture(uTexture, vec2(fragUV.x, 1.0 - fragUV.y));
+  fragColor = sRGBTransferOETF( fragColor );
 }
 `;
 
