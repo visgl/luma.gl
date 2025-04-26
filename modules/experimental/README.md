9adb34d1e838cabcc99f2aca6ce1066b3c9abf10
Ib Green <7025232+ibgreen@users.noreply.github.com>
2025-04-26 10:24:04 -0400

    chore(experimental): Rescaffold the @luma.gl/experimental module (#2375)

---
+++ modules/experimental/README.md
@@ -0,0 +1,10 @@
+# @luma.gl/experimental
+
+Experimental features for luma.gl.
+
+:::warning
+These are experimental features that may change or be removed at any time. Use at your own risk.
+Experimental features are not documented. Refer to comments in code.
+:::
+
+See [luma.gl](http://luma.gl) for documentation.
--- test/modules.ts
+++ test/modules.ts
@@ -8,6 +8,7 @@ luma.log.set('debug', true);
 // Shadertools is a GLSL textual processing library, no actual WebGL dependencies
 import '@luma.gl/shadertools/test';
 import '@luma.gl/effects/test';
+import '@luma.gl/core/test';
 
 // DEVICE INDEPENDENT TESTS
 import '@luma.gl/core/test';
@@ -21,7 +22,7 @@ import '@luma.gl/webgpu/test';
 // CROSS-DEVICE TESTS
 import '@luma.gl/engine/test';
 import '@luma.gl/gltf/test';
-// import '@luma.gl/experimental/test';
+import '@luma.gl/experimental/test';
 
 // EXPERIMENTAL
 import '@luma.gl/arrow/test';
