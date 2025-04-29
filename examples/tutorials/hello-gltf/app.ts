381ca5f6ff210c005f42bd72bbb8c6af5037dfd1
Georgios Karnas <kargeor@hotmail.com>
2025-04-28 23:18:05 -0700

    [hello-gltf] fix model matrix and enable animator

--- examples/tutorials/hello-gltf/app.ts
+++ examples/tutorials/hello-gltf/app.ts
@@ -6,7 +6,7 @@ import {AnimationLoopTemplate, AnimationProps, GroupNode, ModelNode} from '@luma
 import {Device} from '@luma.gl/core';
 import {load} from '@loaders.gl/core';
 import {LightingProps} from '@luma.gl/shadertools';
-import {createScenegraphsFromGLTF} from '@luma.gl/gltf';
+import {createScenegraphsFromGLTF, GLTFAnimator} from '@luma.gl/gltf';
 import {GLTFLoader, postProcessGLTF} from '@loaders.gl/gltf';
 import {Matrix4} from '@math.gl/core';
 
@@ -15,9 +15,15 @@ import {Matrix4} from '@math.gl/core';
 export default class AppAnimationLoopTemplate extends AnimationLoopTemplate {
   device: Device;
   scenes: GroupNode[] = [];
+  animator?: GLTFAnimator;
   center = [0, 0, 0];
-  vantage = [0, 0, 0];
+  cameraPos = [0, 0, 0];
   time: number = 0;
+  options: Record<string, boolean> = {
+    cameraAnimation: true,
+    gltfAnimation: false,
+    pbr: false
+  };
 
   constructor({device, animationLoop}: AnimationProps) {
     super();
@@ -27,8 +33,20 @@ export default class AppAnimationLoopTemplate extends AnimationLoopTemplate {
       throw new Error('This demo is only implemented for WebGL2');
     }
 
-    window.localStorage.lastGltfModel = window.localStorage.lastGltfModel ?? 'Avocado';
-    this.loadGLTF(window.localStorage.lastGltfModel);
+    window.localStorage['lastGltfModel'] ??= 'Avocado';
+    this.loadGLTF(window.localStorage['lastGltfModel']);
+
+    this.initUiOptions();
+  }
+
+  initUiOptions() {
+    Object.keys(this.options).forEach(id => {
+      const checkbox = document.getElementById(id) as HTMLInputElement;
+      checkbox.checked = this.options[id];
+      checkbox.addEventListener('change', e => {
+        this.options[id] = checkbox.checked;
+      });
+    });
 
     const modelSelector = document.getElementById('model-select') as HTMLSelectElement;
 
@@ -37,7 +55,7 @@ export default class AppAnimationLoopTemplate extends AnimationLoopTemplate {
     modelSelector?.addEventListener('change', e => {
       const name = (e.target as HTMLSelectElement).value;
       this.loadGLTF(name);
-      window.localStorage.lastGltfModel = name;
+      window.localStorage['lastGltfModel'] = name;
     });
   }
 
@@ -55,7 +73,7 @@ export default class AppAnimationLoopTemplate extends AnimationLoopTemplate {
     });
 
     modelSelector.append(...options);
-    modelSelector.value = window.localStorage.lastGltfModel;
+    modelSelector.value = window.localStorage['lastGltfModel'];
   }
 
   onFinalize() {
@@ -66,32 +84,36 @@ export default class AppAnimationLoopTemplate extends AnimationLoopTemplate {
     if (!this.scenes?.length) return;
     const renderPass = device.beginRenderPass({clearColor: [0, 0, 0, 1], clearDepth: 1});
 
-    const far = 2 * this.vantage[0];
+    const far = 2 * this.cameraPos[0];
     const near = far / 1000;
     const projectionMatrix = new Matrix4().perspective({fovy: Math.PI / 3, aspect, near, far});
-    const vantage = [
-      this.vantage[0] * Math.sin(0.001 * time),
-      this.vantage[1],
-      this.vantage[2] * Math.cos(0.001 * time)
+    const cameraTime = this.options['cameraAnimation'] ? time : 0;
+    const cameraPos = [
+      this.cameraPos[0] * Math.sin(0.001 * cameraTime),
+      this.cameraPos[1],
+      this.cameraPos[2] * Math.cos(0.001 * cameraTime)
     ];
 
-    this.scenes[0].traverse((node, {worldMatrix}) => {
+    if (this.options['gltfAnimation']) {
+      this.animator?.setTime(time);
+    }
+
+    const viewMatrix = new Matrix4().lookAt({eye: cameraPos, center: this.center});
+
+    this.scenes[0].traverse((node, {worldMatrix: modelMatrix}) => {
       const {model} = node as ModelNode;
 
-      const eye = worldMatrix.transformAsPoint(vantage);
-      const center = worldMatrix.transformAsPoint(this.center);
-      const viewMatrix = new Matrix4().lookAt({eye, center});
       const modelViewProjectionMatrix = new Matrix4(projectionMatrix)
         .multiplyRight(viewMatrix)
-        .multiplyRight(worldMatrix);
+        .multiplyRight(modelMatrix);
 
       model.shaderInputs.setProps({
         lighting: lightSources,
         pbrProjection: {
-          camera: eye,
+          camera: cameraPos,
           modelViewProjectionMatrix,
-          modelMatrix: worldMatrix,
-          normalMatrix: new Matrix4(worldMatrix).invert().transpose()
+          modelMatrix,
+          normalMatrix: new Matrix4(modelMatrix).invert().transpose()
         }
       });
       model.draw(renderPass);
@@ -110,8 +132,9 @@ export default class AppAnimationLoopTemplate extends AnimationLoopTemplate {
     const processedGLTF = postProcessGLTF(gltf);
 
     const options = {pbrDebug: false, imageBasedLightingEnvironment: null, lights: true};
-    const {scenes} = createScenegraphsFromGLTF(this.device, processedGLTF, options);
+    const {scenes, animator} = createScenegraphsFromGLTF(this.device, processedGLTF, options);
     this.scenes = scenes;
+    this.animator = animator;
 
     // Calculate nice camera view
     // TODO move to utility in gltf module
@@ -122,7 +145,7 @@ export default class AppAnimationLoopTemplate extends AnimationLoopTemplate {
       min = min.map((n, i) => Math.min(n, bounds[0][i], bounds[1][i]));
       max = max.map((n, i) => Math.max(n, bounds[0][i], bounds[1][i]));
     });
-    this.vantage = [2 * (max[0] + max[2]), max[1], 2 * (max[0] + max[2])];
+    this.cameraPos = [2 * (max[0] + max[2]), max[1], 2 * (max[0] + max[2])];
     this.center = [0.5 * (min[0] + max[0]), 0.5 * (min[1] + max[1]), 0.5 * (min[2] + max[2])];
 
     canvas.style.opacity = '1';
