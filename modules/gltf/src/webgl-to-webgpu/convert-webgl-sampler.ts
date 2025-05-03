da5f99a715a96a0ccdf829d975a71b62f92b1d3c
Ib Green <ib.green.home@gmail.com>
2025-05-03 13:40:22 -0400

    sampler-tests

--- modules/gltf/src/webgl-to-webgpu/convert-webgl-sampler.ts
+++ modules/gltf/src/webgl-to-webgpu/convert-webgl-sampler.ts
@@ -7,7 +7,20 @@
 import type {SamplerProps} from '@luma.gl/core';
 import {GL} from '@luma.gl/constants';
 
-export function convertSampler(gltfSampler: any): SamplerProps {
+type GLTFSampler = {
+  wrapS?: GL.CLAMP_TO_EDGE | GL.REPEAT | GL.MIRRORED_REPEAT;
+  wrapT?: GL.CLAMP_TO_EDGE | GL.REPEAT | GL.MIRRORED_REPEAT;
+  magFilter?: GL.NEAREST | GL.LINEAR;
+  minFilter?:
+    | GL.NEAREST
+    | GL.LINEAR
+    | GL.NEAREST_MIPMAP_NEAREST
+    | GL.LINEAR_MIPMAP_NEAREST
+    | GL.NEAREST_MIPMAP_LINEAR
+    | GL.LINEAR_MIPMAP_LINEAR;
+};
+
+export function convertSampler(gltfSampler: GLTFSampler): SamplerProps {
   return {
     addressModeU: convertSamplerWrapMode(gltfSampler.wrapS),
     addressModeV: convertSamplerWrapMode(gltfSampler.wrapT),
