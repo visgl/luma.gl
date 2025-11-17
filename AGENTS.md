a3589687ada01f79b7cdee7a26365430fc605119
Ib Green <ib@unfolded.ai>
2025-11-17 16:13:53 -0500

    prettier

--- AGENTS.md
+++ AGENTS.md
@@ -8,7 +8,7 @@
 - Check website build `cd website; yarn; yarn build`
 
 ## Before committing
-- Format code: `yarn lint fix`
+- Always `yarn lint fix` after making changes to ensure that prettier formatting is maintained.
  
 ## Code style
 - TypeScript strict mode
--- modules/webgl/test/adapter/resources/webgl-render-pass.spec.ts
+++ modules/webgl/test/adapter/resources/webgl-render-pass.spec.ts
@@ -2,57 +2,57 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) vis.gl contributors
 
-import test from 'tape-promise/tape'
+import test from 'tape-promise/tape';
 
-import {GL} from '@luma.gl/constants'
-import {WEBGLRenderPass} from '@luma.gl/webgl'
-import {getWebGLTestDevice} from '@luma.gl/test-utils'
+import {GL} from '@luma.gl/constants';
+import {WEBGLRenderPass} from '@luma.gl/webgl';
+import {getWebGLTestDevice} from '@luma.gl/test-utils';
 
 test('WEBGLRenderPass#drawBuffers for framebuffer attachments', async t => {
-  const device = await getWebGLTestDevice()
-  const {gl} = device
+  const device = await getWebGLTestDevice();
+  const {gl} = device;
 
-  const drawBufferCalls: number[][] = []
-  const originalDrawBuffers = gl.drawBuffers.bind(gl)
+  const drawBufferCalls: number[][] = [];
+  const originalDrawBuffers = gl.drawBuffers.bind(gl);
   gl.drawBuffers = ((buffers: number[]) => {
-    drawBufferCalls.push([...buffers])
-    return originalDrawBuffers(buffers as any)
-  }) as typeof gl.drawBuffers
+    drawBufferCalls.push([...buffers]);
+    return originalDrawBuffers(buffers as any);
+  }) as typeof gl.drawBuffers;
 
-  const framebuffer = device.createFramebuffer({colorAttachments: ['rgba8unorm', 'rgba8unorm']})
+  const framebuffer = device.createFramebuffer({colorAttachments: ['rgba8unorm', 'rgba8unorm']});
 
-  const renderPass = new WEBGLRenderPass(device, {framebuffer})
-  renderPass.end()
+  const renderPass = new WEBGLRenderPass(device, {framebuffer});
+  renderPass.end();
 
   t.deepEqual(
     drawBufferCalls[0],
     [GL.COLOR_ATTACHMENT0, GL.COLOR_ATTACHMENT0 + 1],
     'uses framebuffer color attachments as draw buffers'
-  )
+  );
 
-  gl.drawBuffers = originalDrawBuffers
-  framebuffer.destroy()
-  device.destroy()
-  t.end()
-})
+  gl.drawBuffers = originalDrawBuffers;
+  framebuffer.destroy();
+  device.destroy();
+  t.end();
+});
 
 test('WEBGLRenderPass#drawBuffers for default framebuffer', async t => {
-  const device = await getWebGLTestDevice()
-  const {gl} = device
+  const device = await getWebGLTestDevice();
+  const {gl} = device;
 
-  const drawBufferCalls: number[][] = []
-  const originalDrawBuffers = gl.drawBuffers.bind(gl)
+  const drawBufferCalls: number[][] = [];
+  const originalDrawBuffers = gl.drawBuffers.bind(gl);
   gl.drawBuffers = ((buffers: number[]) => {
-    drawBufferCalls.push([...buffers])
-    return originalDrawBuffers(buffers as any)
-  }) as typeof gl.drawBuffers
+    drawBufferCalls.push([...buffers]);
+    return originalDrawBuffers(buffers as any);
+  }) as typeof gl.drawBuffers;
 
-  const renderPass = new WEBGLRenderPass(device, {})
-  renderPass.end()
+  const renderPass = new WEBGLRenderPass(device, {});
+  renderPass.end();
 
-  t.deepEqual(drawBufferCalls[0], [GL.BACK], 'draws to GL.BACK for default framebuffer')
+  t.deepEqual(drawBufferCalls[0], [GL.BACK], 'draws to GL.BACK for default framebuffer');
 
-  gl.drawBuffers = originalDrawBuffers
-  device.destroy()
-  t.end()
-})
+  gl.drawBuffers = originalDrawBuffers;
+  device.destroy();
+  t.end();
+});
