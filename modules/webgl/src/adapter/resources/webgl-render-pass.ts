d89ad926ef20f6efc1e424b77b80032bce995f9b
Ib Green <7025232+ibgreen@users.noreply.github.com>
2025-11-17 15:56:46 -0500

    Fix draw buffers setup for default framebuffer

--- modules/webgl/src/adapter/resources/webgl-render-pass.ts
+++ modules/webgl/src/adapter/resources/webgl-render-pass.ts
@@ -50,7 +50,7 @@ export class WEBGLRenderPass extends RenderPass {
         (_, i) => GL.COLOR_ATTACHMENT0 + i
       );
       this.device.gl.drawBuffers(drawBuffers);
-    } else {
+    } else if (!this.props.framebuffer) {
       this.device.gl.drawBuffers([GL.BACK]);
     }
 
