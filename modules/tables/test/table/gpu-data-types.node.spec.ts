e394b3df6c4427cbf4271ac0431633370a892dcc
Ib Green <ib.green.home@gmail.com>
2026-07-04 07:49:24 -0400

    test(tables): simplify GPUData type coverage

---
+++ modules/tables/test/table/gpu-data-types.node.spec.ts
@@ -0,0 +1,28 @@
+// luma.gl
+// SPDX-License-Identifier: MIT
+// Copyright (c) vis.gl contributors
+
+import {NullDevice} from '@luma.gl/test-utils';
+import {GPUData, type GPUDataView} from '@luma.gl/tables';
+import {expectTypeOf, test} from 'vitest';
+
+test('GPUData infers inline struct field types', () => {
+  const device = new NullDevice({});
+  const buffer = device.createBuffer({byteLength: 64});
+  const data = new GPUData({
+    buffer,
+    length: 2,
+    format: {a: 'sint32', b: 'float32'},
+    layout: 'packed'
+  });
+
+  data satisfies GPUData<{a: 'sint32'; b: 'float32'}, 'packed'>;
+  expectTypeOf(data.getChild('b')).toEqualTypeOf<GPUDataView<'float32'> | null>();
+  expectTypeOf(data.getChild('missing')).toEqualTypeOf<null>();
+  expectTypeOf(data.format?.layout).toEqualTypeOf<'packed' | undefined>();
+
+  const scalarData = new GPUData({buffer, length: 2, format: 'float32'});
+  expectTypeOf(scalarData.format).toEqualTypeOf<'float32' | undefined>();
+
+  buffer.destroy();
+});
