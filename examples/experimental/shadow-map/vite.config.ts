5ee83e15ef21375f92b2aebc035d64bcfcc33a28
Ib Green <ib@openai.com>
2026-07-21 12:03:12 -0400

    docs: Add shadow map quality example (#2747)

---
+++ examples/experimental/shadow-map/vite.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vite';
+
+const alias = {
+  '@luma.gl/core': __dirname + '/../../../modules/core/src',
+  '@luma.gl/engine': __dirname + '/../../../modules/engine/src',
+  '@luma.gl/experimental': __dirname + '/../../../modules/experimental/src',
+  '@luma.gl/shadertools': __dirname + '/../../../modules/shadertools/src',
+  '@luma.gl/webgpu': __dirname + '/../../../modules/webgpu/src'
+};
+
+export default defineConfig({resolve: {alias}, server: {open: true}});
