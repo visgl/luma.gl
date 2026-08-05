b349cb0d3a0483e70966b3d2ad3febf3049e8671
Ib Green <ib.green.home@gmail.com>
2026-08-04 21:04:07 -0400

    feat(experimental): compute GPU-resident vertex degrees

--- modules/experimental/src/lugraph/index.ts
+++ modules/experimental/src/lugraph/index.ts
@@ -6,3 +6,5 @@ export {LuGraph} from './lu-graph';
 export type {LuGraphProps} from './lu-graph';
 export {LuGraphTopology} from './lu-graph-topology';
 export type {LuGraphAdjacency, LuGraphTopologyProps} from './lu-graph-topology';
+export {LuGraphDegree} from './lu-graph-degree';
+export type {LuGraphDegreeDirection, LuGraphDegreeProps} from './lu-graph-degree';
