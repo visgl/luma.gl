08b0f791e526e6fee5c9b665211ed1603430c470
Ib Green <ib.green.home@gmail.com>
2026-08-07 09:22:28 -0400

    fix(experimental): add SPDX headers for luDF analytics

--- modules/experimental/src/ludf/lu-histogram-compiler.ts
+++ modules/experimental/src/ludf/lu-histogram-compiler.ts
@@ -1,6 +1,6 @@
 // luma.gl
 // SPDX-License-Identifier: MIT
-// Copyright (c) vis.gl contributors
+// SPDX-FileCopyrightText: Copyright (c) vis.gl contributors
 
 import {GPUVector, type GPUField, type GPUTypeMap} from '@luma.gl/tables';
 import {type GPUCommandGraph, type GraphDataView} from '../gpu-primitives/gpu-command-graph';
