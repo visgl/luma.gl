5b742e19658b9234b0162979adefcfc4f3e10be0
Ib Green <ib@openai.com>
2026-08-07 12:51:12 -0400

    test(splats): cover hierarchy paths in browser

---
+++ modules/splats/test/splat-browser-coverage.spec.ts
@@ -0,0 +1,7 @@
+// luma.gl
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright (c) vis.gl contributors
+
+import './gpu-splat-graph-interaction.node.spec';
+import './splat-gltf.node.spec';
+import './splat-hierarchy.node.spec';
