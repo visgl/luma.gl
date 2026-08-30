f6e413d5a1a148ee6ca55b1bea598a538662ced7
Ib Green <ib@unfolded.ai>
2026-08-30 16:37:21 -0400

    test: migrate shader tools utility suites to native vitest

--- modules/shadertools/test/lib/glsl-utils/shader-utils.node.spec.ts
+++ modules/shadertools/test/lib/glsl-utils/shader-utils.node.spec.ts
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: Copyright (c) vis.gl contributors
 
-import test from 'test/utils/vitest-tape';
 import {registerShaderUtilsTests} from './shader-utils.spec.shared';
 
-registerShaderUtilsTests(test);
+registerShaderUtilsTests();
