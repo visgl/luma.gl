42af6dab2a48181390814e4b83f914f29dbcf1dd
Ib Green <7025232+ibgreen@users.noreply.github.com>
2026-08-30 18:17:22 -0400

    test: expand shader-tools Vitest migration and close review gap (#3154)

--- modules/shadertools/test/modules/lighting/phong-material.node.spec.ts
+++ modules/shadertools/test/modules/lighting/phong-material.node.spec.ts
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: Copyright (c) vis.gl contributors
 
-import test from 'test/utils/vitest-tape';
 import {registerPhongMaterialTests} from './phong-material.spec.shared';
 
-registerPhongMaterialTests(test);
+registerPhongMaterialTests();
