e50b10a6b28815285d0bb4516c5a6b2c23cce3a6
Ib Green <ib@unfolded.ai>
2026-08-30 16:15:00 -0400

    test: expand native vitest migration across engine utilities

--- modules/engine/test/utils/split-uniforms-and-bindings.node.spec.ts
+++ modules/engine/test/utils/split-uniforms-and-bindings.node.spec.ts
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: Copyright (c) vis.gl contributors
 
-import test from 'test/utils/vitest-tape';
 import {registerSplitUniformsAndBindingsTests} from './split-uniforms-and-bindings.spec.shared';
 
-registerSplitUniformsAndBindingsTests(test);
+registerSplitUniformsAndBindingsTests();
